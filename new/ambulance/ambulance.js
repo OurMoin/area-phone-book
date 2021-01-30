0f05c4b26a2b9dcf72103b21895a3d92c63ba2c7
Moin <moinulislam.dev@gmail.com>
2021-01-30 14:21:22 +0600

    v2.0 alpha

---
+++ new/ambulance/ambulance.js
@@ -0,0 +1,66 @@
+const cardsData = [
+  {
+    photo: "../../img/icon/001-go-shoping.png",
+    name: "Moiul Islam",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bow bazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/002-consumer.png",
+    name: "Sharif Imtaz",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01955446677",
+    location: "Sahi bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/003-online-shopping.png",
+    name: "Asikul Rhaman",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01521216013",
+    location: "Islamia bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/004-customer-service.png",
+    name: "Shanto Mondol",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01622113344",
+    location: "Bow bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/001-go-shoping.png",
+    name: "Moiul Islam",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bow bazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/002-consumer.png",
+    name: "Sharif Imtaz",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01955446677",
+    location: "Sahi bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/003-online-shopping.png",
+    name: "Asikul Rhaman",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01521216013",
+    location: "Islamia bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../img/icon/004-customer-service.png",
+    name: "Shanto Mondol",
+    facebook: "https://www.facebook.com/",
+    phoneNumber: "01622113344",
+    location: "Bow bazer",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  }
+];
