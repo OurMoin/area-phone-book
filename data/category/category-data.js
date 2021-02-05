d0e9fa2cf9c9f30e033f24aea62bbf8908ffc574
Moin <moinulislam.dev@gmail.com>
2021-02-05 21:05:46 +0600

    image path change

--- data/category/category-data.js
+++ data/category/category-data.js
@@ -1,16 +1,16 @@
 const categorysData = [
     {
         link: "emergency-services",
-        img: "resources/images/icon/category/emergency-services.png",
+        img: "../../resources/images/icon/category/emergency-services.png",
         alt: "emergency services",
         name: "জরুরী সেবা",
         tp1: "এ্যাম্বুলেন্স",
         tp2: "হাঁসপাতাল",
         tp3: "ফায়ার সার্ভিস"
-    }/*,
+    },
     {
         link: "#",
-        img: "resources/images/icon/category/medical-services.png",
+        img: "../../resources/images/icon/category/medical-services.png",
         alt: "medical services",
         name: "চিকিৎসা সেবা",
         tp1: "ফার্মেসী",
@@ -19,11 +19,11 @@ const categorysData = [
     },
     {
         link: "#",
-        img: "resources/images/icon/category/shop.png",
+        img: "../../resources/images/icon/category/shop.png",
         alt: "shop",
         name: "দোকান",
         tp1: "সুপার সপ",
         tp2: "মিনি সুপার সপ",
         tp3: "পাইকারী দোকান"
-    }*/
+    }
 ];
