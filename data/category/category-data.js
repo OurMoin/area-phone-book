e96faf75129904c23a7504936073e4c926a3dfa5
shantomondol <shantomondol816@gmail.com>
2021-02-06 11:26:32 +0600

    add category-data

--- data/category/category-data.js
+++ data/category/category-data.js
@@ -1,5 +1,4 @@
-const categorysData = [
-    {
+const categorysData = [{
         link: "emergency-services",
         img: "../../resources/images/icon/category/emergency-services.png",
         alt: "emergency services",
@@ -25,5 +24,68 @@ const categorysData = [
         tp1: "সুপার সপ",
         tp2: "মিনি সুপার সপ",
         tp3: "পাইকারী দোকান"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/parlor-salon.png",
+        alt: "parlor-salon",
+        name: "পার্লার / সেলুন",
+        tp1: "পার্লার",
+        tp2: "সেলুন",
+        tp3: "কসমিটিক্স এর দোকান"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/studio-computer-shop.png",
+        alt: "studio-computer-shop",
+        name: "স্টুডিও / কম্পিউটার দোকান",
+        tp1: "স্টুডিও",
+        tp2: "কম্পিউটার দোকান",
+        tp3: "লাইব্রেরি"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/car-stand.png",
+        alt: "car-stand",
+        name: "গাড়ী স্ট্যান্ড",
+        tp1: "CNG স্ট্যান্ড",
+        tp2: "বাস স্ট্যান্ড",
+        tp3: "ট্রেন স্টেশন"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/transportation-services.png",
+        alt: "transportation-services",
+        name: "পরিবহন সেবা",
+        tp1: "পেট্রোল পাম্প",
+        tp2: "রেন্ট এ কার",
+        tp3: "পিকাপ"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/food-corner.png",
+        alt: "food-corner",
+        name: "ফুড কর্নার / খাবার দোকান",
+        tp1: "হোটেল",
+        tp2: "রেস্তরাঁ",
+        tp3: "বিরিয়ানির দোকান"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/servicing-center.png",
+        alt: "servicing-center",
+        name: "সার্ভিসিং সেন্টার",
+        tp1: "টিভি",
+        tp2: "এসি / ফ্রিজ",
+        tp3: "কম্পিউটার"
+    },
+    {
+        link: "#",
+        img: "../../resources/images/icon/category/education-services.png",
+        alt: "education-services",
+        name: "শিক্ষা সেবা",
+        tp1: "স্কুল",
+        tp2: "কলেজ",
+        tp3: "মাদ্রসা"
     }
 ];
