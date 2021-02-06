fdce83360c5430f00b5ff7f5813c4ab8b4dcbd09
Moin <moinulislam.dev@gmail.com>
2021-02-06 13:05:18 +0600

    demo user added

--- data/users/ambulance.js
+++ data/users/ambulance.js
@@ -4,7 +4,15 @@ const usersData = [
     name: "Moiul Islam",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01303002226",
-    location: "Bow bazar",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "Sakila Akter",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
     map: "https://goo.gl/maps/4uVocYHH8xVrECex6"
   }
 ];
