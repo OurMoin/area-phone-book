59d6f1a34d5d65ffcd4b2af6306b716c651009e9
Moin <moinulislam.dev@gmail.com>
2021-02-06 20:13:06 +0600

    added sub cat icons

--- components/user.js
+++ components/user.js
@@ -6,7 +6,7 @@ function userTemplate(user) {
           <div class="card-body cleartfix">
             <div class="media align-items-stretch">
               <div class="align-self-center">
-                <img src="${user.photo}" alt="${user.name}" class="icon user-img">
+                <img src="${user.photo}" alt="${user.name}" class="manu-img">
               </div>
               <div class="media-body">
                 <h4><a href="${user.facebook}" target="_blank">${user.name}</a></h4>
@@ -16,7 +16,7 @@ function userTemplate(user) {
               </div>
               <div class="align-self-center">
                 <a href="tel:${user.phoneNumber}">
-                  <i class="call-loc fas fa-phone-alt"></i>
+                  <i class="call-icon fas fa-phone-alt"></i>
                 </a>
                 <a href="${user.map}" target="_blank">
                   <i class="fas fa-map-marker-alt"></i>
