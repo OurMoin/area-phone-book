d45f531a0c040fe264de17731ca510e9b19b1274
Moin <moinulislam.dev@gmail.com>
2021-02-06 13:37:49 +0600

    alt remove from cat data

--- data/category/category-data.js
+++ data/category/category-data.js
@@ -1,7 +1,6 @@
 const categorysData = [{
         link: "emergency-services",
         img: "../../resources/images/icon/category/emergency-services.png",
-        alt: "emergency services",
         name: "জরুরী সেবা",
         tp1: "এ্যাম্বুলেন্স",
         tp2: "হাঁসপাতাল",
@@ -10,7 +9,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/medical-services.png",
-        alt: "medical services",
         name: "চিকিৎসা সেবা",
         tp1: "ফার্মেসী",
         tp2: "ডাক্তার বসেন",
@@ -19,7 +17,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/shop.png",
-        alt: "shop",
         name: "দোকান",
         tp1: "সুপার সপ",
         tp2: "মিনি সুপার সপ",
@@ -28,7 +25,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/parlor-salon.png",
-        alt: "parlor-salon",
         name: "পার্লার / সেলুন",
         tp1: "পার্লার",
         tp2: "সেলুন",
@@ -37,7 +33,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/studio-computer-shop.png",
-        alt: "studio-computer-shop",
         name: "স্টুডিও / কম্পিউটার দোকান",
         tp1: "স্টুডিও",
         tp2: "কম্পিউটার দোকান",
@@ -46,7 +41,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/car-stand.png",
-        alt: "car-stand",
         name: "গাড়ী স্ট্যান্ড",
         tp1: "CNG স্ট্যান্ড",
         tp2: "বাস স্ট্যান্ড",
@@ -55,7 +49,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/transportation-services.png",
-        alt: "transportation-services",
         name: "পরিবহন সেবা",
         tp1: "পেট্রোল পাম্প",
         tp2: "রেন্ট এ কার",
@@ -64,7 +57,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/food-corner.png",
-        alt: "food-corner",
         name: "ফুড কর্নার / খাবার দোকান",
         tp1: "হোটেল",
         tp2: "রেস্তরাঁ",
@@ -73,7 +65,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/servicing-center.png",
-        alt: "servicing-center",
         name: "সার্ভিসিং সেন্টার",
         tp1: "টিভি",
         tp2: "এসি / ফ্রিজ",
@@ -82,7 +73,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/education-services.png",
-        alt: "education-services",
         name: "শিক্ষা সেবা",
         tp1: "স্কুল",
         tp2: "কলেজ",
@@ -91,7 +81,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/gift-corner.png",
-        alt: "gift-corner",
         name: "গিফট কর্নার",
         tp1: "গিফট এর দোকান",
         tp2: "1 to 99 দোকান",
@@ -100,7 +89,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/market.png",
-        alt: "market",
         name: "মার্কেট",
         tp1: "কাঁচা বাজার",
         tp2: "পোশাকের বাজার",
@@ -109,7 +97,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/training-center.png",
-        alt: "training-center",
         name: "প্রশিক্ষণ কেন্দ্র",
         tp1: "কম্পিউটার ট্রেনিং",
         tp2: "ড্রাইভিং ট্রেনিং",
@@ -118,7 +105,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/decorator.png",
-        alt: "decorator",
         name: "ডেকোরেটস",
         tp1: "",
         tp2: "",
@@ -127,7 +113,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/tourist-spot.png",
-        alt: "tourist-spot",
         name: "ঘোরার জায়গা",
         tp1: "",
         tp2: "",
@@ -136,7 +121,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/rental-services.png",
-        alt: "rental-services",
         name: "ভাড়া সেবা",
         tp1: "",
         tp2: "",
@@ -145,7 +129,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/security-services.png",
-        alt: "security-services",
         name: "সুরক্ষা সেবা",
         tp1: "",
         tp2: "",
@@ -154,7 +137,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/civic-service.png",
-        alt: "civic-service",
         name: "নাগরিক সেবা",
         tp1: "",
         tp2: "",
@@ -163,7 +145,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/banking-services.png",
-        alt: "banking-services",
         name: "ব্যাংকিং সেবা",
         tp1: "",
         tp2: "",
@@ -172,7 +153,6 @@ const categorysData = [{
     {
         link: "#",
         img: "../../resources/images/icon/category/religious-place.png",
-        alt: "religious-place",
         name: "ধার্মিক স্থান",
         tp1: "",
         tp2: "",
