f183832d77e0aa2bdc4e7799381d740a68870811
Moin <moinulislam.dev@gmail.com>
2021-02-08 16:08:19 +0600

    help card change to slider

--- components/help.js
+++ components/help.js
@@ -7,9 +7,39 @@ const helpsData = [
 
 function helpTemplate(help) {
     return `
-    <img src="${help.img}" alt="Help Card" class="help-card">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100 help-card" src="${help.img}" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100 help-card" src="${help.img}" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100 help-card" src="${help.img}" alt="Third slide">
+            </div>
+        </div>        
+    </div>
     `;
 }
+
+
+// Previous & Next button for slaider
+/* 
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+</a>
+*/
 
 
 document.getElementById("help").innerHTML = `
