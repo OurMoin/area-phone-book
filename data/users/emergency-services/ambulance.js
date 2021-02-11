5f853da735334b8b172eb4eacee9b7f3dd6511b2
shantomondol <shantomondol816@gmail.com>
2021-02-11 20:20:08 +0600

    data entry-1

--- data/users/emergency-services/ambulance.js
+++ data/users/emergency-services/ambulance.js
@@ -1,7 +1,7 @@
 const usersData = [
   {
     photo: "../../resources/images/icon/user/man.png",
-    name: "Moinul Islam",
+    name: "Niom Computer Training",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01303002226",
     location: "Bowbazar",
@@ -10,7 +10,79 @@ const usersData = [
   },
   {
     photo: "../../resources/images/icon/user/woman.png",
-    name: "Sakila Akter",
+    name: "DBBL Agent Banking",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "হ্রদয় সাইকেল স্টোর",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "লাবনী ক্রোকারিজ স্টোর",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "মমতাজ ডেকোরেটর",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "জনসেবা ফার্মেসী",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "অনুপম লাইব্রেরি",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "তৃপ্তি হোটেল ও রেস্টুরেন্ট",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "অভিজাত ফার্নিচার মার্ট",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "মায়ের দোয়া গ্রিল",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01521216013",
     location: "Noyamati",
