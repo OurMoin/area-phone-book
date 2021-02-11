da349a91bd06b4c2dab7b5ed4d3b21df7bd52486
shantomondol <shantomondol816@gmail.com>
2021-02-11 20:34:40 +0600

    data entry-2

--- data/users/emergency-services/ambulance.js
+++ data/users/emergency-services/ambulance.js
@@ -1,7 +1,7 @@
 const usersData = [
   {
     photo: "../../resources/images/icon/user/man.png",
-    name: "Niom Computer Training",
+    name: "নিয়ম কম্পিউটার ট্রেনিং",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01303002226",
     location: "Bowbazar",
@@ -10,7 +10,7 @@ const usersData = [
   },
   {
     photo: "../../resources/images/icon/user/woman.png",
-    name: "DBBL Agent Banking",
+    name: "ডিবিবিএল এজেন্ট ব্যাংকিং",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01521216013",
     location: "Noyamati",
@@ -83,6 +83,96 @@ const usersData = [
   {
     photo: "../../resources/images/icon/user/woman.png",
     name: "মায়ের দোয়া গ্রিল",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "স্টামফোর্ড চাইল্ড ইন্সটিটিউট",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "মদিনাতুল উলূম মাদ্রাসা ও এতিমখানা",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "জয়া বিউটি পার্লার ও কসমেটিক্স",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "মায়ের দোয়া কমিউনিটি সেন্টার",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "ক্যামব্রিজ কিন্ডারগার্টেন স্কুল",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "হক রাইস এজেন্সি",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/man.png",
+    name: "ভাই ভাই ফার্মেসি",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01303002226",
+    location: "Bowbazar",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "মেসার্স তাহমিনা এন্টারপ্রাইজ",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "মাদবর হার্ডওয়্যার স্যানিটারি এন্ড অটো পার্টস",
+    facebook: "https://www.facebook.com/moinulislam.dev/",
+    phoneNumber: "01521216013",
+    location: "Noyamati",
+    map: "https://goo.gl/maps/4uVocYHH8xVrECex6",
+    detail: "Details about this service"
+  },
+  {
+    photo: "../../resources/images/icon/user/woman.png",
+    name: "ছালমান-আল ফারসি জামে মসজিদ",
     facebook: "https://www.facebook.com/moinulislam.dev/",
     phoneNumber: "01521216013",
     location: "Noyamati",
