e9a16a9efaf92203f96c4df9a9a94d97524a7a28
shantomondol <shantomondol816@gmail.com>
2021-02-14 12:51:58 +0600

    some change in about file

---
+++ about/footer.js
@@ -0,0 +1,47 @@
+const footersData = [
+    {}
+];
+
+
+function footerTemplate(footer) {
+    return `
+    <div class="blog-header">
+        <div class="container">
+            <div class="row flex-nowrap justify-content-between align-items-center">
+                <div class="col-6 pt-1">
+                    <ul>
+                        <li><a href="#" title="Privacy"><i class="fas fa-user-secret"></i></a></li>
+                        <li><a href="#" title="Terms"><i class="fas fa-file-alt"></i></a></li>
+                        <li><a href="#" title="Sponsor"><i class="fas fa-handshake"></i></a></li>
+                        <li><a href="../" title="Home"><i class="fas fa-house-user"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-6 d-flex justify-content-end align-items-center">
+                    <ul>
+                        <li><a href="tel:01798899911" target="_blank"><i class="fas fa-phone-square-alt"></i></a>
+                        </li>
+                        <li><a href="mailto:areaphonebook@gmail.com" target="_blank"><i
+                                    class="fas fa-envelope"></i></a></li>
+                        <li><a href="https://www.facebook.com/groups/Paglabashi" target="_blank"><i
+                                    class="fas fa-users"></i></a></li>
+                        <li><a href="https://www.facebook.com/AreaPhoneBook" target="_blank"><i
+                                    class="fab fa-facebook-square"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center copyright">
+                    <p>Area PhoneBook &#169; 2021</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+
+document.getElementById("footer").innerHTML = `
+    ${footersData.map(footerTemplate).join("")}
+  `;
+
+
