fcb191ee21af6bb1dced7a577ef4fb96fc67fe99
Moin <moinulislam.dev@gmail.com>
2021-02-25 21:34:58 +0600

    new card added image zoom baki

---
+++ demo-cards2/userData.js
@@ -0,0 +1,14 @@
+const userData = [
+  {
+    id: 1,
+    photo: "https://www.areaphonebook.com/resources/images/icon/category/training-center.png",
+    name: "নিয়ম কম্পিউটার ট্রেনিং",
+    number: "01303002226",
+    wordNo: "6",
+    location: "প্যারাডাইস সিটি",
+    locationLink: "https://www.google.com/search?q=প্যারাডাইস সিটি",
+    detail: "খোলা থাকেঃ<br> সকাল ৯ টা - রাত ৯ টা<br> কোর্স সমুহঃ<br>মাইক্রোসফট অফিস, গ্রাফিক ডিজাইন, ওয়েব ডিজাইন",
+    postLink: "http://fb.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
+  }
+];
