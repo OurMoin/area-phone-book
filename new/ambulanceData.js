fde96fb83dd59546c9e16b72660d12776918099e
Moin <moinulislam.dev@gmail.com>
2021-03-08 13:01:27 +0600

    aaded new website

---
+++ new/ambulanceData.js
@@ -0,0 +1,35 @@
+const userData = [
+  {
+    id: 1,
+    photo: "https://images.jdmagicbox.com/comp/jaipur/x9/0141px141.x141.130414141842.e2x9/catalogue/ganesh-ambulance-mansarovar-jaipur-ambulance-services-76ar5rpq2q.jpg?clr=#333333",
+    name: "Khaled Ambulance Service",
+    number: "01933246577",
+    wordNo: "5",
+    location: "প্যারাডাইস সিটি",
+    detail: "সার্ভিস টাইম:<br> সকাল ৯ টা - রাত ৯ টা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "https://content3.jdmagicbox.com/comp/hyderabad/j8/040pxx40.xx40.181106113503.h6j8/catalogue/sai-krishna-ambulance-santosh-nagar-hyderabad-ambulance-services-0ncwwtwlyc.jpg?clr=382e2e",
+    name: "Lamyea Ambulance Services",
+    number: "01710070011",
+    wordNo: "6",
+    location: "বউ বাজার",
+    detail: "সার্ভিস টাইম:<br> ২৪ ঘন্টা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "https://images.jdmagicbox.com/comp/hyderabad/z8/040pxx40.xx40.190424161805.i1z8/catalogue/mj-ambulance-services-hyderabad-ambulance-services-kqekom0qv0.jpg?clr=#264026",
+    name: "Conscious Ambulance Service",
+    number: "01912603094",
+    wordNo: "4",
+    location: "নয়ামাটি",
+    detail: "সার্ভিস টাইম:<br> সকাল ৯ টা - রাত ৯ টা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
---
+++ new/caegoryData.js
@@ -0,0 +1,43 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "https://www.areaphonebook.com/resources/images/icon/category/emergency-services.png",
+      name: "জরুরী সেবা",
+      subCategory:
+        [
+          {
+            name: "এ্যাম্বুলেন্স",
+            link: "ambulance.html"
+          },
+          {
+            name: "হাঁসপাতাল",
+            link: "#"
+          },
+          {
+            name: "ফায়ার সার্ভিস",
+            link: "#"
+          }
+        ]
+    },
+    {
+      id: 2,
+      photo: "https://www.areaphonebook.com/resources/images/icon/category/training-center.png",
+      name: "প্রশিক্ষণ কেন্দ্র",
+      subCategory:
+        [
+          {
+            name: "কম্পিউটার ট্রেনিং",
+            link: "#"
+          },
+          {
+            name: "ড্রাইভিং ট্রেনিং",
+            link: "#"
+          },
+          {
+            name: "ভাষা ট্রেনিং",
+            link: "#"
+          }
+        ]
+    }
+  ];
+  
