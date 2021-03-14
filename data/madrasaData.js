2c124a81587aae591d966bd72c75b2f68894082a
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-14 17:11:32 +0600

    data entry-10

---
+++ data/madrasaData.js
@@ -0,0 +1,57 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/madrasa/madrasa.png",
+    name: "মদিনাতুল উলূম মাদ্রাসা ও এতিমখানা",
+    number: "01726783592",
+    wordNo: "৫",
+    location: "উত্তর শাহী মহল্লা",
+    detail: "হেফজ খানা ও মহিলা মাদ্রাসা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/madrasa/madrasa.png",
+    name: "জামিয়া তা'লীমিয়া মাদ্রাসা ঢাকা",
+    number: "01703073400",
+    wordNo: "৪",
+    location: "আদর্শ নগর",
+    detail: "হেফজ খানা ও মাদ্রাসা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/madrasa/madrasa.png",
+    name: "আয়েশা সিদ্দিকা রা. মহিলা মাদ্রাসা",
+    number: "01732804216",
+    wordNo: "৪",
+    location: "আর্দশ নগর",
+    detail: "হেফজ খানা ও মাদ্রাসা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/madrasa/madrasa.png",
+    name: "আলেকজান হামিদিয়া হামিয়াতুস সুন্নাহ মাদ্রাসা",
+    number: "Hidden",
+    wordNo: "৫",
+    location: "দক্ষিণ রসুলপুর, বড় মসজিদ এর পাশে",
+    detail: "হেফজ খানা ও মাদ্রাসা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/madrasa/madrasa.png",
+    name: "আলেকজান হামিদিয়া হামিয়াতুস সুন্নাহ মাদ্রাসা",
+    number: "Hidden",
+    wordNo: "৫",
+    location: "দক্ষিণ রসুলপুর, বড় মসজিদ এর পাশে",
+    detail: "হেফজ খানা ও মাদ্রাসা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
