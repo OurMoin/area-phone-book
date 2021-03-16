3e4716358e043f45ecacfad93865e3a235a83803
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-16 21:55:02 +0600

    data entry in hardware-sanitary-electronics file

---
+++ data/hardware-sanitary-electronicsData.js
@@ -0,0 +1,156 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "মাদবর হার্ডওয়্যার স্যানিটারি এন্ড অটো পার্টস",
+    number: "01610717869",
+    wordNo: "৫",
+    location: "নিশ্চিন্ত পূর",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "মেসার্স মিজান স্যানিটারি হার্ডওয়্যার এন্ড ইলেকট্রিক",
+    number: "01931968588",
+    wordNo: "৪",
+    location: "আর্দশ নগর",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "আর. এস ইলেকট্রনিক্স",
+    number: "01893799669",
+    wordNo: "৪",
+    location: "আদর্শ নগর",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "মেসার্স খান ইলেকট্রিক ও হার্ডওয়্যার",
+    number: "01912059199",
+    wordNo: "৪",
+    location: "আদর্শ নগর",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "আল্লাহর দান ইলেকট্রনিক, হার্ডওয়ার",
+    number: "01922444913",
+    wordNo: "৫",
+    location: "ভাঙ্গাপুল, রসুলপুর",
+    detail: "এখানে ইলেকট্রনিক এবং হার্ডওয়ার এর সকল পণ্য পাওয়া যায় ওয়ালটন এবং ভীসণ এর সকল পণ্য পাওয়া যায়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "এম এ মান্নান হার্ডওয়ার",
+    number: "01961229822",
+    wordNo: "৫",
+    location: "ভাঙ্গাপুল, রসুলপুর",
+    detail: "ইলেকট্রিক এন্ড প্লাস্টিক পণ্য",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 7,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "জননী ইলেকট্রনিক্স মিডিয়া",
+    number: "01964778395",
+    wordNo: "৫",
+    location: "ভাঙ্গাপুল, রসুলপুর",
+    detail: "ফার্নিচার এবং ওয়ালটনের সকল ইলেকট্রনিক পণ্য পাওয়া যায়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "রাফি ইলেকট্রনিক্স এন্ড হার্ডওয়ার",
+    number: "Hidden",
+    wordNo: "৫",
+    location: "পশ্চিম রসুলপুর, মোল্লা টাওয়ার, রসুলপুর",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 9,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "শরীয়তপুর হার্ডওয়ার এন্ড ইলেকট্রনিক্স",
+    number: "01707332091",
+    wordNo: "৫",
+    location: "মধ্য রসুলপুর",
+    detail: "হার্ডওয়ার এন্ড ইলেকট্রনিক্স সকল পণ্য পাওয়া যায়। ওয়াল্টন এর সকল পণ্য পাওয়া যায়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 10,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "রাজধানী গ্লাস এন্ড হার্ডওয়ার স্যানিটারি",
+    number: "01918131775",
+    wordNo: "৫",
+    location: "শাহীবাজার মার্কেট, শাহীবাজার",
+    detail: "সকল সেবা, হোম সেবা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 11,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "রাজধানী হার্ডওয়্যার",
+    number: "01918502660",
+    wordNo: "৫",
+    location: "দেলপাড়া রোড, নয়ামাটি জামে মসজিদ সংলগ্ন",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 12,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "জুয়েল হার্ডওয়্যার এন্ড ইলেকট্রনিক্স",
+    number: "01968734459",
+    wordNo: "৫",
+    location: "দেলপাড়া বাজার, কমল উদ্দিন প্লাজা , দেলপাড়া",
+    detail: "Details about this service",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 13,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "বাহার হার্ডওয়ার এন্ড সেনেটারী",
+    number: "01915867582",
+    wordNo: "৫",
+    location: "দেলপাড়া উচ্চ বিদ্যালয় সংলগ্ন",
+    detail: "হার্ডওয়্যার, সেনেটারী পণ্য। বার্জার, এশিয়ান পেইন্টস এর ডিলার।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 14,
+    photo: "../cdn/hardware-sanitary-electronics/hardware-sanitary-electronics.png",
+    name: "বাহার হার্ডওয়ার এন্ড সেনেটারী",
+    number: "01915867582",
+    wordNo: "৫",
+    location: "দেলপাড়া উচ্চ বিদ্যালয় সংলগ্ন",
+    detail: "হার্ডওয়্যার, সেনেটারী পণ্য। বার্জার, এশিয়ান পেইন্টস এর ডিলার।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
