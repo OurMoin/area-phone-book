df17bf44de76c89e20e99eb6cbcfe3557a1b58ab
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-17 20:26:08 +0600

    data entry in studio file

---
+++ data/studioData.js
@@ -0,0 +1,90 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/studio/studio.png",
+    name: "মা ডিজিটাল স্টুডিও",
+    number: "01587133461",
+    wordNo: "৫",
+    location: "পশ্চিম দৌলতপুর ব্রিজ সংলগ্ন কদমতলী পাকার মাথা।",
+    detail: "বিভিন্ন অনুষ্ঠানের ভিডিও এবং স্টিল ফটো শুট করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/studio/studio.png",
+    name: "মা ডিজিটাল ফটো স্টুডিও",
+    number: "01913836871",
+    wordNo: "৫",
+    location: "দক্ষিণ রসুলপুর, রেললাইন সংলগ্ন",
+    detail: "ফটো শুট করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/studio/studio.png",
+    name: "হাসবি ডিজিটাল স্টুডিও",
+    number: "01911856376",
+    wordNo: "৫",
+    location: "ভাঙ্গাপুল, রসুলপুর",
+    detail: "ছবি তোলা ও কম্পোজিং করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/studio/studio.png",
+    name: "আলিফ স্টুডিও",
+    number: "01915577882",
+    wordNo: "৫",
+    location: "আমতলা-শাহীবাজার রোড, আমতলা",
+    detail: "ছবি তোলা ও ছবি থেকে ছবি বের করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/studio/studio.png",
+    name: "মারিয়া ডিজিটাল স্টুডিও",
+    number: "01918198064",
+    wordNo: "৫",
+    location: "দেলপাড়া উচ্চ বিদ্যালয় সংলগ্ন",
+    detail: "ছবি তোলা, ছবি থেকে ছবি, বিভিন্ন অনুষ্ঠানের ভিডিও এর কাজ করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/studio/studio.png",
+    name: "ম্যাগা ডিজিটাল স্টুডিও",
+    number: "01611177749",
+    wordNo: "৫",
+    location: "নয়ামাটি দেলপাড়া রোড",
+    detail: "ছবি তোলা, ছবি থেকে ছবি এবং বিভিন্ন অনুষ্ঠানের ভিডিও ও অবিজ্ঞ ক্যামেরাম্যান ধারা ছবি তোলা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 7,
+    photo: "../cdn/studio/studio.png",
+    name: "ছবিমেলা ডিজিটাল স্টুডিও",
+    number: "01916416075",
+    wordNo: "৫",
+    location: "দক্ষিণ নয়ামাটি",
+    detail: "ছবি তোলা, ছবি থেকে ছবি, এবং বিভিন্ন অনুষ্ঠানের ভিডিও ও ছবি তোলার কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/studio/studio.png",
+    name: "ডায়না ডিজিটাল স্টুডিও",
+    number: "01718940862",
+    wordNo: "৫",
+    location: "এ এস এম সুপার মার্কেট, পাগলা",
+    detail: "ছবি তোলা, ছবি থেকে ছবি, এবং বিভিন্ন অনুষ্ঠানের ভিডিও ও ছবি তোলার কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
