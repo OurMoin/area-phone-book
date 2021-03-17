bae5accae50ad8fb2973c1da74723333c19d2b8a
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-17 16:22:35 +0600

    data entry in doctor-sat-down file

---
+++ data/doctor-sat-downData.js
@@ -0,0 +1,123 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "আল- ইহসান ফার্মা",
+    number: "01672664134",
+    wordNo: "৫",
+    location: "কুমিল্লা সমিতির মার্কেট, শাহী বাজার",
+    detail: "ডাঃ মোঃ আবদুন নূর সায়েম।<br> এম. বি. বি. এস, বি. সি. এস (স্বাস্থ্য)<br> রোগী দেখার সময়ঃ বিকাল ৫.৩০ মিঃ হইতে রাত ৯.০০ টা পর্যন্ত।<br> শনি, সোম ও বুধবার",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "শহিদুল মেডিকেল হল",
+    number: "01916382877",
+    wordNo: "৫",
+    location: "বাবির বাজার",
+    detail: "ডাঃ কাজী আশ্ররাফ (মাতুইল)<br> ডাক্তার বসেনঃ সন্ধা ৭ টা থেকে ৯ টা পর্যন্ত।<br> শনিবার, সোমবার, বুধবার।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "কেয়ার মেডিসিন কর্নার",
+    number: "01630243900",
+    wordNo: "৫",
+    location: "নয়ামাটি জামে মসজিদ সংলগ্ন, নয়ামাটি",
+    detail: "শুধু দাতের ডাক্তার বসে,<br>ডাক্তার বসেনঃ ৬টা - ১০টা পর্যন্ত",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "ডাঃ মুজিবুর রহমান",
+    number: "01819271396",
+    wordNo: "৬",
+    location: "এ এস এম সুপার মার্কেট, পাগলা",
+    detail: "ডাঃ মুজিবুর রহমান (মেডিসিন বিশেষজ্ঞ)<br> ডাক্তার বসেনঃ বিকাল ৪ টা থেকে রাত ৯.৩০ টা পর্যন্ত",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "হিমেল ডেন্টাল কেয়ার ",
+    number: "01687014219",
+    wordNo: "৬",
+    location: "এ এস এম সুপার মার্কেট, পাগলা",
+    detail: "দাঁতের ডাক্তার বসে,<br> ডাক্তার বসেনঃ দুপুর ৩ টা থেকে ৯টা পর্যন্ত",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "জনসেবা ফার্মেসি",
+    number: "01917566006",
+    wordNo: "৫",
+    location: "আমতলা মোড়",
+    detail: "চোখের ডাক্তার বসে - প্রতি মঙ্গলবার ১০ টা থেকে ৩ টা।<br>দাতের ডাক্তার বসে - প্রতি বৃহস্পতিবার ৩ টা থেকে ৬ টা।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 7,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "ডক্টরস ডেন্টাল সার্জারী",
+    number: "01779881842",
+    wordNo: "৪",
+    location: "দেলপাড়া বাজার, দেলপাড়া",
+    detail: "দাঁতের ডাক্তার বসে।<br> ডাক্তার বসেনঃ প্রতিদিন বিকাল ৫টা থেকে রাত ১০টা পর্যন্ত",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "দেওয়ার ফার্মেসি",
+    number: "01911798538",
+    wordNo: "৫",
+    location: "ইকরা স্কুল সংলগ্ন, কুসুমবাগ",
+    detail: "ডাক্তার বসে, শিশু, গাইনি, নাক-কান গলা, মেডিসিন বিশেষজ্ঞ",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 9,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "জনসেবা ফার্মেসী",
+    number: "01902322132",
+    wordNo: "৫",
+    location: "নুরবাগ জামে মসজিদ সংলগ্ন, নুরবাগ",
+    detail: "ডাক্তার বসে, মেডিসিন বিশেষজ্ঞ",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 10,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "ইবনে সিনা মেডিসিন কর্নার",
+    number: "01916204618",
+    wordNo: "৫",
+    location: "নুরবাগ জামে মসজিদ সংলগ্ন, নুরবাগ",
+    detail: "ডাক্তার বসে চোখের, মেডিসিন বিশেষজ্ঞ ও চক্ষু",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 11,
+    photo: "../cdn/doctor-sat-down/doctor-sat-down.png",
+    name: "রিমা ডেন্টাল কেয়ার",
+    number: "01748585601",
+    wordNo: "৫",
+    location: "হাজী ফজলুল হক মার্কেট, ভাঙার-পুল",
+    detail: "দাঁতের ডাক্তার বসে।<br>রবিবার ও বুধবার।<br> ৫টা থেকে রাত ১০টা পর্যন্ত (আগে যোগাযোগ করবেন)",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
