c8d3442733f7ae4da1bf401bcef4526e13622523
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-17 16:26:13 +0600

    some text change in pharmacy file

--- data/pharmacyData.js
+++ data/pharmacyData.js
@@ -17,7 +17,7 @@ const userData = [
     number: "01677255264",
     wordNo: "৫",
     location: "নিশ্চিন্ত পূর",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -28,7 +28,7 @@ const userData = [
     number: "01880118922",
     wordNo: "৫",
     location: "শরীফবাগ, শাহীবাজার",
-    detail: "Details about this service",
+    detail: "সকল প্রকারের মেডিসিন পাওয়া যায়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -50,7 +50,7 @@ const userData = [
     number: "01712649188",
     wordNo: "৫",
     location: "দক্ষিণ রসুলপুর, মাদ্রাসা রোড",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -61,7 +61,7 @@ const userData = [
     number: "Hidden",
     wordNo: "৫",
     location: "ভাঙ্গাপুল, রসুলপুর",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -72,7 +72,7 @@ const userData = [
     number: "01886626026",
     wordNo: "৫",
     location: "ভাঙ্গাপুল, রসুলপুর",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -83,7 +83,7 @@ const userData = [
     number: "01916061648",
     wordNo: "৫",
     location: "নুরবাগ পুরাতন সি এন জি স্ট্যান্ড, নুরবাগ",
-    detail: "Details about this service",
+    detail: "সকল প্রকারের মেডিসিন পাওয়া যায়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -94,7 +94,7 @@ const userData = [
     number: "01916204618",
     wordNo: "৫",
     location: "নুরবাগ জামে মসজিদ সংলগ্ন, নুরবাগ",
-    detail: "ডাক্তার বসে চোখের, মেডিসিন, চক্ষু",
+    detail: "সকল প্রকারের মেডিসিন পাওয়া যায়<br> ডাক্তার বসে চোখের, মেডিসিন, চক্ষু",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -204,7 +204,7 @@ const userData = [
     number: "01630243900",
     wordNo: "৫",
     location: "নয়ামাটি জামে মসজিদ সংলগ্ন, নয়ামাটি",
-    detail: "শুধু দাতের ডাক্তার বসে, ৬টা - ১০টা। সকল ধরনের চিকিৎসা করা হয়। ওষুধ হোম ডেলিভারি করা হয়। সরাসরী কোম্পানি থেকে ক্রয়-ক্রীত ওষুধ পাওয়া যায়। ",
+    detail: "সকল ধরনের চিকিৎসা করা হয়। ওষুধ হোম ডেলিভারি করা হয়। সরাসরী কোম্পানি থেকে ক্রয়-ক্রীত ওষুধ পাওয়া যায়। ",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -248,7 +248,7 @@ const userData = [
     number: "01680034159",
     wordNo: "৫",
     location: "মধ্য রসুলপুর, কমিউনিটি সেন্টার সংলগ্ন",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -270,7 +270,7 @@ const userData = [
     number: "01711052055",
     wordNo: "৫",
     location: "জসিম মার্কেট, রসুলপুর",
-    detail: "Details about this service",
+    detail: "সকল প্রকার ওষুধ পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
