9c2cf54c6fdce530b4d3434783bf0fa14a5e4bd6
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-17 12:18:21 +0600

    data entry in parlor file

---
+++ data/parlorData.js
@@ -0,0 +1,35 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/parlor/parlor.png",
+    name: "জয়া বিউটি পার্লার ও কসমেটিক্স",
+    number: "01610717908",
+    wordNo: "৫",
+    location: "উত্তর শাহী মহল্লা",
+    detail: "বিউটি পার্লার ও কসমেটিক্স এর দোকান",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/parlor/parlor.png",
+    name: "কনফিডেন্স হারবাল বিউটি পার্লার এন্ড কসমেটিক সেন্টার",
+    number: "01676037240",
+    wordNo: "৫",
+    location: "ওয়ায়েস করনী আদর্শ নগর",
+    detail: "হারবাল বিউটি পার্লার এন্ড কসমেটিক সেন্টার",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/parlor/parlor.png",
+    name: "Women's land Beauty salon & Spa",
+    number: "01909547850",
+    wordNo: "৫",
+    location: "রসুলপুর জসিম মার্কেট সংলগ্ন",
+    detail: "Beauty salon & Spa",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
