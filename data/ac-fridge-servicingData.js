97cf4e425b51d56a7f68ea54c97a1b2e458a038d
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-19 21:05:35 +0600

    data entry in ac-fridge-servicing file

---
+++ data/ac-fridge-servicingData.js
@@ -0,0 +1,35 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/ac-fridge-servicing/ac-fridge-servicing.png",
+    name: "মায়ের দোয়া রেফ্রিজারেশন এন্ড ওয়ার্কশপ",
+    number: "01994956071",
+    wordNo: "৫",
+    location: "দেলপাড়া বাজার সংলগ্ন",
+    detail: "এসি, ফ্রিজ, মাইক্রোওভেন, ওয়াশিং মেশিন সার্ভিসিং করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/ac-fridge-servicing/ac-fridge-servicing.png",
+    name: "সারা রেফ্রিজারেটর",
+    number: "01706368048",
+    wordNo: "৬",
+    location: "বউবাজার শাহীবাজার রোড, বউবাজার",
+    detail: "ফ্রিজ, এসি সার্ভিসিং করা হয়, হোম সার্ভিস করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/ac-fridge-servicing/ac-fridge-servicing.png",
+    name: "মেসার্স ওসমান রেফ্রিজারেশন, ওয়ার্কশপ",
+    number: "01710580609",
+    wordNo: "৬",
+    location: "দেলপাড়া রোড, কামালপুর, পাগলা",
+    detail: "সকল প্রকার এসি, ফ্রিজ সার্ভিসিং করা হয়। ( হোম সার্ভিস করা হয় )",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
