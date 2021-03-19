4d75f719425c6728dd3955779b290bb3929cf452
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-19 18:02:35 +0600

    data entry in cameraman file

---
+++ data/cameramanData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/cameraman/cameraman.png",
+    name: "মোঃ রাসেল",
+    number: "01687133461",
+    wordNo: "5",
+    location: "কদমতলী পাকার মাথা",
+    detail: "স্টিল ফটো শুট এন্ড ভিডিও করা হয়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
