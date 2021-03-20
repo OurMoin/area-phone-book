989a879c79ba3088400f43f940cf1ecb4ae8f934
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-20 12:19:43 +0600

    data entry in wholesale-store file

---
+++ data/wholesale-storeData.js
@@ -0,0 +1,35 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "মেসার্স সিকদার ট্রেডার্স",
+    number: "01678713811",
+    wordNo: "৬",
+    location: "বউবাজার",
+    detail: "সকল পাইকারি পণ্য পাওয়া যায়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "তাসফিয়া ট্রেডার্স",
+    number: "01631281616",
+    wordNo: "৫",
+    location: "উত্তর-পূর্ব শাহী মহল্লা",
+    detail: "সকল পাইকারি পণ্য পাওয়া যায়",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "মেসার্স জনি ফ্লাওয়ার মিলস",
+    number: "01791058215,01679600590",
+    wordNo: "৬",
+    location: "ডি এন রোড, পাগলা",
+    detail: "সকল প্রকারের পাইকারি পন্য পাওয়া যায়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
