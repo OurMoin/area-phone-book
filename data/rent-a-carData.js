5e065e8757d761fc859c1b57a4e919f94e8d92da
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-21 20:48:10 +0600

    data entry in district-truck file data change in rent-a-car file

--- data/rent-a-carData.js
+++ data/rent-a-carData.js
@@ -13,17 +13,6 @@ const userData = [
   {
     id: 2,
     photo: "../cdn/rent-a-car/rent-a-car.png",
-    name: "অনিক ট্রাভেল",
-    number: "01986334181",
-    wordNo: "6",
-    location: "তাল-তলা, পাগলা",
-    detail: "সকল প্রকার ডিসট্রিক্ট ট্রাক পাওয়া যায়",
-    postLink: "http://fb.com/areaPhonebook",
-    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
-  {
-    id: 3,
-    photo: "../cdn/rent-a-car/rent-a-car.png",
     name: "মীর রেন্ট-এ-কার",
     number: "01631092669",
     wordNo: "৫",
@@ -33,7 +22,7 @@ const userData = [
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
-    id: 4,
+    id: 3,
     photo: "../cdn/rent-a-car/rent-a-car.png",
     name: "হোসেন রেন্ট-এ-কার",
     number: "01952835050",
