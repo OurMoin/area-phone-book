e3154eccff0e238dfe8f348fb8f32740c52c4242
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-26 21:44:09 +0600

    data entry doctor-lab file

---
+++ data/doctor-labData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/doctor-lab/doctor-lab.png",
+    name: "নিউ ডক্টরস ডায়াগনস্টিক ল্যাব",
+    number: "01772200965",
+    wordNo: "৬",
+    location: "হাজী নুর ইসলাম সুপার মার্কেট, ২য় তলা, পাগলা",
+    detail: "ডাক্তার বসেঃ উল্লেখিত নাম্বারে যোগাযোগ করুন।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
--- data/rent-a-carData.js
+++ data/rent-a-carData.js
@@ -31,5 +31,16 @@ const userData = [
     detail: "হাইস গাড়ি ভাড়া দেওয়া হয়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/rent-a-car/rent-a-car.png",
+    name: "আব্দুর রব মিয়ার গ্যারেজ",
+    number: "01687774539",
+    wordNo: "৫",
+    location: "নয়ামাটি, ভাবি বাজার রোড।",
+    detail: "সকল প্রকার গাড়ি ভাড়া দেওয়া হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
