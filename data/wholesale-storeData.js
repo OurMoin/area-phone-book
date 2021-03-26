633dfe936e440726cfba8ab45d3c7c7d4fd2aec8
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-26 21:21:23 +0600

    data add in wholesale-store file

--- data/wholesale-storeData.js
+++ data/wholesale-storeData.js
@@ -42,5 +42,16 @@ const userData = [
     detail: "পাইকারি দোকান",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "মিথুন এন্টারপ্রাইজ",
+    number: "01979126418",
+    wordNo: "৫",
+    location: "নয়ামাটি বাজার",
+    detail: "বসুন্ধরা এর সয়াবিন তেল, আটা, ময়দা, সুজি, মেঘনা গ্রুপ এর দুধ, পানি, আটা, ময়দা, সুজি।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
