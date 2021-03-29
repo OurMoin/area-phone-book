ba94c4c247e196e741155037cfe09ef6e5f3a867
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-29 12:31:47 +0600

    data entry in telecom-center file

---
+++ data/telecom-centerData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/telecom-center/arsad-telecom.jpg",
+    name: "আরসাদ টেলিকম",
+    number: "01811114551",
+    wordNo: "null",
+    location: "নুরবাগ ভুইগড় রোড, নুরবাগ",
+    detail: "বিকাশ, রকেট এবং নিত্য প্রয়োজনীয় ইলেকট্রিক পণ্য পাওয়া যায়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
