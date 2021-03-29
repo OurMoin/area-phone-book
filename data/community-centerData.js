4b6ab226e80610fdd159d5ad43f8a7dec117fbfb
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-29 13:03:46 +0600

    add data in community-center file

--- data/community-centerData.js
+++ data/community-centerData.js
@@ -6,7 +6,18 @@ const userData = [
     number: "01719648377",
     wordNo: "৫",
     location: "উত্তর-পূর্ব শাহী মহল্লা",
-    detail: "Details about this service",
+    detail: "সকল আনুষ্ঠানের জন্য যোগাযোগ করুন।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/community-center/mugdha-snigdha-community-center.jpg",
+    name: "মুগ্ধ স্নিগ্ধ কমিউনিটি সেন্টার",
+    number: "01910707912",
+    wordNo: "৪",
+    location: "দেলপাড়া (টাওয়ার পাড়া)",
+    detail: "এখানে গায়ে হলুদ, বিয়ে, বৌভাত, জন্মদিন, সুন্নতে খাৎনা সহ সকল আনুষ্ঠানের জন্য যোগাযোগ করুন।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
