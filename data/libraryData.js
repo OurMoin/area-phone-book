d5ac620b974f1ae8267ce8e5bd32d60ea765083b
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-29 21:32:49 +0600

    data add in library file

--- data/libraryData.js
+++ data/libraryData.js
@@ -12,7 +12,7 @@ const userData = [
   },
   {
     id: 2,
-    photo: "../cdn/library/library.png",
+    photo: "../cdn/library/nazrul-library.jpg",
     name: "নজরুল লাইব্রেরী",
     number: "01922229023",
     wordNo: "৫",
@@ -34,10 +34,10 @@ const userData = [
   },
   {
     id: 4,
-    photo: "../cdn/library/library.png",
+    photo: "../cdn/library/sm-library.jpg",
     name: "এস এম লাইব্রেরী",
     number: "01749242430",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "ভাঙ্গাপুল, রসুলপুর",
     detail: "ইসলামিক লাইব্রেরী",
     postLink: "http://fb.com/areaPhonebook",
@@ -56,13 +56,24 @@ const userData = [
   },
   {
     id: 6,
-    photo: "../cdn/library/library.png",
+    photo: "../cdn/library/gauchia-library-and-stationery.jpg",
     name: "গাউছিয়া লাইব্রেরী এন্ড ষ্টেশনারী",
     number: "0194943927",
-    wordNo: "৫",
+    wordNo: "৪",
     location: "দেলপাড়া উচ্চ বিদ্যালয় সংলগ্ন",
     detail: "লাইব্রেরী এন্ড ষ্টেশনারী পণ্য ও ফটোকপি করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"  
+  },
+  {
+    id: 7,
+    photo: "../cdn/library/riya-library.jpg",
+    name: "রিয়া লাইব্রেরী এন্ড কম্পিউটারস্",
+    number: "01933141414",
+    wordNo: "৬",
+    location: "পাগলা উচ্চ বিদ্যালয় সংলগ্ন, নয়ামাটি, পাগলা",
+    detail: "বই বিক্রি, ফটোকপি, লেমিনেটিং, লাইব্রেরী সকল পণ্য পাওয়া যায় এবং কম্পিউটারের যাবতীয় কাজ করা হয়।",
+    postLink: "https://www.facebook.com/riyalibrarybd/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"  
   }
 ];
