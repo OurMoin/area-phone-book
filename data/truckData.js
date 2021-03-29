210bf6ace3b3187c558c6a7de3a4ec891ae5a122
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-29 21:10:13 +0600

    data entry in truck file

---
+++ data/truckData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/truck/robin-and-brothers.jpg",
+    name: "রবিন এন্ড ব্রাদার্স",
+    number: "01910707912",
+    wordNo: "৫",
+    location: "শাহীবাজার, পাগলা",
+    detail: "লোকাল ট্রাক ভাড়া দেওয়া হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
