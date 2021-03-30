e45bdbc6cb63d1352555360ea5ad6aab49f9c7d8
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-30 17:35:03 +0600

    data entry in salon file

---
+++ data/salonData.js
@@ -0,0 +1,14 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/salon/rajiv-nayan-hair-design.jpg",
+    name: "রাজিব নয়ন হেয়ার ডিজাইন",
+    number: "01933308887",
+    wordNo: "৫",
+    location: "শরিফবাগ মোড়, কদমতলী রোড",
+    detail: "সেলুনের যাবতীয় সকল কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
+  
