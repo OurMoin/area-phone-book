91f5824cb7d7cfe822e23d006f0467574d751d5f
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-30 17:29:46 +0600

    data entry in home-teacher file

---
+++ data/home-teacherData.js
@@ -0,0 +1,14 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/home-teacher/md-maruf.jpg",
+    name: "মোঃ মারুফ",
+    number: "01926149448",
+    wordNo: "৫",
+    location: "শাহী মহল্লা",
+    detail: "ক্লাস ৬ - ক্লাস ১০ (ব্যবসায়ী বিভাগ) হোম টিচার",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
+  
