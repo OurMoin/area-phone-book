c0effed8e0e0571ced553a92440f55c6bdfd0e13
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-03-30 17:59:20 +0600

    data add in biryani-shop file

--- data/biryani-shopData.js
+++ data/biryani-shopData.js
@@ -20,5 +20,16 @@ const userData = [
     detail: "সকালের নাশতা, দুপুরের খাবার ও রাতে মুরগীর গ্রিল, হালিম, বিরিয়ানি, নান রুটি পাওয়া যায়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/biryani-shop/kazi-biryani-house.jpg",
+    name: "কাজী বিরিয়ানি ঘর",
+    number: "01683128172",
+    wordNo: "৫",
+    location: "নয়ামাটি দেলপাড়া রোড, নয়ামাটি ডাল সংলগ্ন",
+    detail: "সুস্বাদু বিরিয়ানি, ভুনা খিচুরি, মোরগ পোলাউ পাওয়া যায়। বিভিন্ন অনুষ্ঠানের জন্য অর্ডার সরবরাহ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
