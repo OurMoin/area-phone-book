21739efc1de3e17bc8963e653df555054b792d2e
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-08 19:13:46 +0600

    change wordno and photo in thai-aluminum-and-glassData file

--- data/thai-aluminum-and-glassData.js
+++ data/thai-aluminum-and-glassData.js
@@ -23,10 +23,10 @@ const userData = [
   },
   {
     id: 3,
-    photo: "../cdn/thai-aluminum-and-glass/thai-aluminum-and-glass.png",
+    photo: "../cdn/thai-aluminum-and-glass/neha-thai-aluminum-glass-and-ss.jpg",
     name: "নেহার থাই এ্যালুমিনিয়াম গ্লাস এন্ড এস. এস",
     number: "0191487766",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "দেলপাড়া রোড, নয়ামাটি",
     detail: "থাই এ্যালুমিনিয়াম গ্লাস এন্ড এস. এস এর সকল কাজ করা হয়",
     postLink: "http://fb.com/areaPhonebook",
@@ -37,21 +37,32 @@ const userData = [
     photo: "../cdn/thai-aluminum-and-glass/thai-aluminum-and-glass.png",
     name: "আল্লাহর দান থাই এ্যালুমিনিয়াম এন্ড কাচ ঘর",
     number: "01993441151",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "দেলপাড়া রোড, বাইতুল ফালাহ মসজিদ সংলগ্ন",
     detail: "থাই ও এ্যালুমিনিয়াম এর সকল কাজ করা হয়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
-    id: 4,
+    id: 5,
     photo: "../cdn/thai-aluminum-and-glass/thai-aluminum-and-glass.png",
     name: "মোল্লা গ্লাস এন্ড থাই এ্যালুমিনিয়াম",
     number: "01826663010",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "মর্ডান মার্কেট, কামালপুর, পাগলা",
     detail: "থাই ও এ্যালুমিনিয়াম এর সকল কাজ করা হয়",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  }
+  },
+  {
+    id: 6,
+    photo: "../cdn/thai-aluminum-and-glass/rajadhani-glass-and-hardware-sanitary.jpg",
+    name: "রাজধানী গ্লাস এন্ড হার্ডওয়ার স্যানিটারি",
+    number: "01918131775",
+    wordNo: "৫",
+    location: "শাহীবাজার মার্কেট, শাহীবাজার",
+    detail: "সকল সেবা, হোম সেবা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
 ];
