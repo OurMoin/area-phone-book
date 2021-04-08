d03838b5b7ea439d66b87c36b1a293734aaec2eb
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-08 18:14:43 +0600

    add data in home-teacher data file

--- data/home-teacherData.js
+++ data/home-teacherData.js
@@ -1,6 +1,17 @@
 const userData = [
   {
     id: 1,
+    photo: "../cdn/home-teacher/female-home-teacher.png",
+    name: "ফারিয়া জাহান পুষ্প",
+    number: "01302880960",
+    wordNo: "৫",
+    location: "পূর্ব রসুলপুর",
+    detail: "ক্লাস ১ - ক্লাস ৫ এর সকল বিষয় পড়ানো হয়।",
+    postLink: "https://www.facebook.com/aalina.angel.54",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
     photo: "../cdn/home-teacher/md-maruf.jpg",
     name: "মোঃ মারুফ",
     number: "01926149448",
