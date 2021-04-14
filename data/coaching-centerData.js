2bae60f133f900bee19fc64154b4e2317f909b1f
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 20:04:43 +0600

    text change in coaching-centerData file

--- data/coaching-centerData.js
+++ data/coaching-centerData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01685322989",
     wordNo: "৬",
     location: "মধ্য রসুলপুর, জসিম মার্কেট সংলগ্ন",
-    detail: "৫- ১২ শ্রেনীর সকল বিষয় পড়ানো হয়",
+    detail: "৫- ১২ শ্রেনীর সকল বিষয় পড়ানো হয়।",
     postLink: "https://www.facebook.com/sbeducarezone/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01608060915",
     wordNo: "৬",
     location: "বড় মসজিদ সংলগ্ন (দেলোয়ার মেম্বার বাড়ি), মধ্য রসুলপুর",
-    detail: "৩য় শ্রেণী থেকে দ্বাদশ শ্রেণীর সকল বিষয় পড়ানো হয়",
+    detail: "৩য় শ্রেণী থেকে দ্বাদশ শ্রেণীর সকল বিষয় পড়ানো হয়।",
     postLink: "https://www.facebook.com/watch/udayan.ac.4/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fudayan.ac.4%2Fvideos%2F160341869245864%2F&width=500&show_text=false&height=282&appId"
   },
@@ -28,7 +28,7 @@ const userData = [
     number: "01927106412",
     wordNo: "৬",
     location: "প্যারাডাইস সিটি, পাগলা রেলস্টেশন সংলগ্ন",
-    detail: "৬ষ্ঠ থেকে অনার্স গণিত, ইংরেজি, ICT, অর্থনীতি, হিসাব বিজ্ঞানের সকল বিষয় পড়ানো হয়",
+    detail: "৬ষ্ঠ থেকে অনার্স গণিত, ইংরেজি, ICT, অর্থনীতি, হিসাব বিজ্ঞানের সকল বিষয় পড়ানো হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -39,7 +39,7 @@ const userData = [
     number: "01718479907",
     wordNo: "৫",
     location: "নয়ামাটি, মুসলিম পাড়া",
-    detail: "ইকরা আদর্শ বিদ্যানিকেতন সহকারী শিক্ষক ( গণিত )",
+    detail: "ইকরা আদর্শ বিদ্যানিকেতন সহকারী শিক্ষক ( গণিত )।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -50,7 +50,7 @@ const userData = [
     number: "01969700990",
     wordNo: "৫",
     location: "আর্দশ নগর",
-    detail: "Details about this service",
+    detail: "সকল বিষয় পড়ানো হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   },
@@ -61,7 +61,7 @@ const userData = [
     number: "01995544427",
     wordNo: "৬",
     location: "নয়ামাটি কেন্দ্রীয় মসজিদ সংলগ্ন, নয়ামাটি, পাগলা",
-    detail: "একাদশ-দ্বাদশ ও বিশ্ববিদ্যালয় ভর্তি কোচিং (ঢাকা বিশ্ববিদ্যালয়ের শিক্ষার্থীদের দ্বারা পরিচালিত)",
+    detail: "একাদশ-দ্বাদশ ও বিশ্ববিদ্যালয় ভর্তি কোচিং (ঢাকা বিশ্ববিদ্যালয়ের শিক্ষার্থীদের দ্বারা পরিচালিত)।",
     postLink: "https://www.facebook.com/QuantumAC2016",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FQuantumAC2016%2Fvideos%2F2886614154955179%2F&width=500&show_text=false&height=282&appId"
   }
