fa69ac3acc98079548409e175d0efbd2b1ed0e31
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 22:10:54 +0600

    add text in crockeryData file

--- data/crockeryData.js
+++ data/crockeryData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01815709903",
     wordNo: "৫",
     location: "আমতলা মোড়",
-    detail: "প্লাস্টিকের পন্য ও সিলভারের হাড়ি-পাতিল পাওয়া যায়",
+    detail: "প্লাস্টিকের পন্য ও সিলভারের হাড়ি-পাতিল পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
