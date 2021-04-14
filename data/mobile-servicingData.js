cf1c755db5664ed1c1c4fe44c4cee7a3d743b791
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 18:18:21 +0600

    text change in mobile-servicing file

--- data/mobile-servicingData.js
+++ data/mobile-servicingData.js
@@ -1,23 +1,23 @@
 const userData = [
   {
     id: 1,
-    photo: "../cdn/mobile-servicing/mobile-servicing.png",
+    photo: "../cdn/mobile-servicing/bismillah-mobile-servicing-center.jpg",
     name: "বিসমিল্লাহ মোবাইল সার্ভিসিং সেন্টার",
     number: "01830646675",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "হাজী ফজলুল হক মার্কেট ২য় তলা,ভাঙ্গাপুল, রসুলপুর",
-    detail: "সব রকমের মোবাইল সার্ভিসিং করা হয়",
+    detail: "সব রকমের মোবাইল সার্ভিসিং করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 2,
-    photo: "../cdn/mobile-servicing/mobile-servicing.png",
+    photo: "../cdn/mobile-servicing/sohail-servicing-center.jpg",
     name: "সোহেল সার্ভিসিং সেন্টার",
     number: "01827279264",
     wordNo: "৬",
     location: "বউবাজার, বাজার সংলগ্ন",
-    detail: "সব ধরনের ফোন সার্ভিসিং করা হয় এবং ফোনের  সকল মালামাল পাওয়া যায়",
+    detail: "সব ধরনের ফোন সার্ভিসিং করা হয় এবং ফোনের  সকল মালামাল পাওয়া যায়।<br> কম্পিউটার কম্পোজ, কালার ও লেজার প্রিন্ট, ফটোকপি, লেমিনেটিং, ৫ মিনিটে রঙিন ছবি, ভিডিও প্রোগাম, ইমেল, ছবি থেকে ছবি ও মেমোরি লোড করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
