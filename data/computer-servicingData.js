08f2598db63b143483729e4627246c5749a2007a
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 19:20:59 +0600

    data entry in computer-servicing file

---
+++ data/computer-servicingData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/computer-servicing/best-computer-training.jpg",
+    name: "বেষ্ট কম্পিউটার ট্রেনিং",
+    number: "01673068354",
+    wordNo: "৪",
+    location: "দেলপাড়া উচ্চ বিদ্যালয় সংলগ্ন",
+    detail: "কম্পিউটার ট্রেনিং, কম্পিউটার সার্ভিসিং ও কম্পিউটারের সকল সরঞ্জাম পাওয়া যায়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
