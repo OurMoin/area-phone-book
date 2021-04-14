b738ae6b8d0de5510f5097815e9e0ab70cd9f4da
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 20:53:21 +0600

    add wordNo in driving-trainingData file

--- data/driving-trainingData.js
+++ data/driving-trainingData.js
@@ -4,9 +4,9 @@ const userData = [
     photo: "../cdn/driving-training/driving-training.png",
     name: "মা বাবার দোয়া কার ড্রাইভিং ট্রেনিং",
     number: "01941724006",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "পাগলা রেলস্টেশন সংলগ্ন।",
-    detail: "Details about this service",
+    detail: "ড্রাইভিং প্রশিক্ষণ দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   }
