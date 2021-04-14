0c273ae9a80b72fab673821e14f68b76dc016eeb
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 21:22:46 +0600

    data add in wholesale-storeData and dealer-pointsData file

--- data/wholesale-storeData.js
+++ data/wholesale-storeData.js
@@ -1,12 +1,12 @@
 const userData = [
   {
     id: 1,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/wholesale-store/messrs-sikder-traders.jpg",
     name: "মেসার্স সিকদার ট্রেডার্স",
     number: "01678713811",
     wordNo: "৬",
     location: "বউবাজার",
-    detail: "সকল পাইকারি পণ্য পাওয়া যায়",
+    detail: "সকল পাইকারি পণ্য পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,18 +17,18 @@ const userData = [
     number: "01631281616",
     wordNo: "৫",
     location: "উত্তর-পূর্ব শাহী মহল্লা",
-    detail: "সকল পাইকারি পণ্য পাওয়া যায়",
+    detail: "সকল পাইকারি পণ্য পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 3,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/wholesale-store/messrs-johnny-flower-mills.jpg",
     name: "মেসার্স জনি ফ্লাওয়ার মিলস",
-    number: "01791058215,01679600590",
+    number: "01791058215",
     wordNo: "৬",
     location: "ডি এন রোড, পাগলা",
-    detail: "সকল প্রকারের পাইকারি পন্য পাওয়া যায়।",
+    detail: "01679600590<br> সকল প্রকারের পাইকারি পন্য পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -39,18 +39,7 @@ const userData = [
     number: "01742155141",
     wordNo: "৫",
     location: "নিশ্চিন্ত পূর",
-    detail: "পাইকারি দোকান",
-    postLink: "http://fb.com/areaPhonebook",
-    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
-  {
-    id: 5,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
-    name: "মিথুন এন্টারপ্রাইজ",
-    number: "01979126418",
-    wordNo: "৫",
-    location: "নয়ামাটি বাজার",
-    detail: "বসুন্ধরা এর সয়াবিন তেল, আটা, ময়দা, সুজি, মেঘনা গ্রুপ এর দুধ, পানি, আটা, ময়দা, সুজি।",
+    detail: "পাইকারি দোকান।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
