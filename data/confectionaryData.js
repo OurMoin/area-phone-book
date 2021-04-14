df1881fe645396a8110dac6f48ce03328469c013
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-14 21:48:14 +0600

    add photo in confectionaryData file

--- data/confectionaryData.js
+++ data/confectionaryData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01684291504",
     wordNo: "৪",
     location: "হাজী আহসান উদ্দিন সুপার মার্কেট, ১ম তলা, আদর্শ নগর",
-    detail: "কনফেকশনারী",
+    detail: "কনফেকশনারীর যাবতীয় জিনিস পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,29 +17,29 @@ const userData = [
     number: "01842620887",
     wordNo: "৪",
     location: "আর্দশ নগর",
-    detail: "মুদি, কনফেকশনারী, কসমেটিক, ষ্টেশনারী",
+    detail: "মুদি, কনফেকশনারী, কসমেটিক, ষ্টেশনারী।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 3,
-    photo: "../cdn/confectionary/confectionary.png",
+    photo: "../cdn/confectionary/haji-food-suites-and-bakery.jpg",
     name: "হাজী ফুড সুইস্ট এন্ড বেকারি",
     number: "01748271952",
-    wordNo: "৫",
+    wordNo: "৪",
     location: "দেলপাড়া বাজার, দেলপাড়া",
-    detail: "মিষ্টি ও বেকারি এর সকল পন্য পাওয়া যাই",
+    detail: "মিষ্টি ও বেকারি এর সকল পন্য পাওয়া যাই।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 4,
-    photo: "../cdn/confectionary/confectionary.png",
+    photo: "../cdn/confectionary/ms-tabassum-enterprise.jpg",
     name: "এম এস তাবাসসুম এন্টারপ্রাইজ",
     number: "01817081248",
     wordNo: "৬",
     location: "হাজী ইউনুস মার্কেট, পাগলা",
-    detail: "মিনি সুপার শপ এবং বেকারির সকল পন্য পাওয়া যাই",
+    detail: "মিনি সুপার শপ এবং বেকারির সকল পন্য পাওয়া যাই।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
