5334b460017474576460f4cbc66a86a9aca14551
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 17:22:30 +0600

    text add in cake-houseData file

--- data/cake-houseData.js
+++ data/cake-houseData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01916218253",
     wordNo: "৬",
     location: "আফসার করিম প্লাজা, ২য় তলা, পাগলা",
-    detail: "ওয়াডারি কেক বানানো হয়",
+    detail: "জন্মদিন ও বিভিন্ন অনুষ্ঠানের জন্য ওয়াডারি কেক বানানো হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
