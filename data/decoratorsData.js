a727788d00793a146855228fabc833c168764721
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 19:21:14 +0600

    add text in decorators Data file

--- data/decoratorsData.js
+++ data/decoratorsData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01923682241",
     wordNo: "৫",
     location: "আমতলা মোড়",
-    detail: "বিয়ে এবং অন্যান্য অনুষ্ঠানের প্যান্ডেল ও স্টেজ করা হয়",
+    detail: "বিয়ে এবং অন্যান্য অনুষ্ঠানের প্যান্ডেল ও স্টেজ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01920993042",
     wordNo: "৪",
     location: "আদর্শ নগর",
-    detail: "বিয়ে এবং অন্যান্য অনুষ্ঠানের প্যান্ডেল ও স্টেজ করা হয়",
+    detail: "বিয়ে এবং অন্যান্য অনুষ্ঠানের প্যান্ডেল ও স্টেজ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
