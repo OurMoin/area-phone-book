609ec6beda22bc849b7b97a5055c1962033b2a3b
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 19:35:21 +0600

    text add in community-centerData file

--- data/community-centerData.js
+++ data/community-centerData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01719648377",
     wordNo: "৫",
     location: "উত্তর-পূর্ব শাহী মহল্লা",
-    detail: "সকল আনুষ্ঠানের জন্য যোগাযোগ করুন।",
+    detail: "এখানে গায়ে হলুদ, বিয়ে, বৌভাত, জন্মদিন, সুন্নতে খাৎনা সহ সকল আনুষ্ঠানের জন্য যোগাযোগ করুন।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
