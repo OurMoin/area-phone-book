13be1e6cb3e6fd9b65a5621adf391fb6ba14e671
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 17:06:36 +0600

    data text change in restaurant file

--- data/restaurantData.js
+++ data/restaurantData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01714275316",
     wordNo: "৫",
     location: "শাহীবাজার কবরস্থান সংলগ্ন, শাহীবাজার",
-    detail: "রেস্টুরেন্ট এবং চাইনিজ এর সকল খাবার পাওয়া যায়",
+    detail: "ফাস্টফুড এন্ড রেস্টুরেন্ট এবং চাইনিজ এর সকল খাবার পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,18 +17,18 @@ const userData = [
     number: "01984547441",
     wordNo: "৪",
     location: "হাজী আহসান উদ্দিন সুপার মার্কেট, ২য় তলা, আদর্শ নগর",
-    detail: "মানসম্মত ও সুস্বাদু ফাস্টফুড পাওয়া যায়",
+    detail: "মানসম্মত ও সুস্বাদু ফাস্টফুড পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 3,
-    photo: "../cdn/restaurant/restaurant.png",
+    photo: "../cdn/restaurant/ahm-tasty-food.jpg",
     name: "AHM Tasty Food",
     number: "01934411439",
     wordNo: "৪",
     location: "আর্দশ নগর",
-    detail: "Fast food",
+    detail: "ফাস্টফুড এন্ড রেস্টুরেন্ট এর সকল খাবার পাওয়া যায়।",
     postLink: "https://www.facebook.com/ahm.tastyfood",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -39,21 +39,21 @@ const userData = [
     number: "01623511197",
     wordNo: "৫",
     location: "আমতলা মোড়",
-    detail: "সকালের নাশতা, দুপুরের খাবার ও রাতে মুরগীর গ্রিল, হালিম, বিরিয়ানি, নান রুটি পাওয়া যায়",
+    detail: "সকালের নাশতা, দুপুরের খাবার ও রাতে মুরগীর গ্রিল, হালিম, বিরিয়ানি, নান রুটি পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
-  {
-    id: 5,
-    photo: "../cdn/restaurant/generation-restaurant-and-cafe.jpg",
-    name: "প্রজন্ম রেস্টুরেন্ট এন্ড ক্যাফে",
-    number: "Hidden",
-    wordNo: "৫",
-    location: "শাহীবাজার, আমতলা রোড",
-    detail: "রেস্টুরেন্ট এন্ড ক্যাফে",
-    postLink: "http://fb.com/areaPhonebook",
-    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
+  // {
+  //   id: 5,
+  //   photo: "../cdn/restaurant/generation-restaurant-and-cafe.jpg",
+  //   name: "প্রজন্ম রেস্টুরেন্ট এন্ড ক্যাফে",
+  //   number: "Hidden",
+  //   wordNo: "৫",
+  //   location: "শাহীবাজার, আমতলা রোড",
+  //   detail: "রেস্টুরেন্ট এন্ড ক্যাফে",
+  //   postLink: "http://fb.com/areaPhonebook",
+  //   videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  // },
   {
     id: 6,
     photo: "../cdn/restaurant/restaurant.png",
@@ -61,7 +61,7 @@ const userData = [
     number: "01710614422",
     wordNo: "৪",
     location: "দেলপাড়া কলেজ রোড",
-    detail: "চাইনিজ রেষ্টুরেন্ট",
+    detail: "চাইনিজ এর সকল খাবার পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -72,7 +72,7 @@ const userData = [
     number: "01851596976",
     wordNo: "৬",
     location: "মুসলিম মার্কেট, ডি এন রোড, পাগলা",
-    detail: "ইন্ডিয়ান এন্ড চাইনিজ খাবার ও ফাস্ট ফুড, ড্রিংস পাওয়া যায়। হোম ডেলিভারি করা হয়।<br>প্রতিদিন সকাল ১০ টা থেকে রাত ১১ টা পর্যন্ত খোলা থাকে",
+    detail: "ইন্ডিয়ান এন্ড চাইনিজ খাবার ও ফাস্ট ফুড, ড্রিংস পাওয়া যায়। হোম ডেলিভারি করা হয়।<br>প্রতিদিন সকাল ১০ টা থেকে রাত ১১ টা পর্যন্ত খোলা থাকে।",
     postLink: "https://www.facebook.com/grandarea.bd/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fgrandarea.bd%2Fvideos%2F436528117576241%2F&width=500&show_text=false&height=282&appId"
   }
