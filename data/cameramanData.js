1c2e586634d0ed6e0e3642a2c490589ad3b75470
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 18:32:22 +0600

    photo add and text add in studio-computer-shop categorysData file

--- data/cameramanData.js
+++ data/cameramanData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01687133461",
     wordNo: "null",
     location: "কদমতলী পাকার মাথা",
-    detail: "স্টিল ফটো শুট এন্ড ভিডিও করা হয়",
+    detail: "স্টিল ফটো শুট এন্ড ভিডিও করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
