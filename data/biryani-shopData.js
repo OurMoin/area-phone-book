9a8bf853381a0c62915a09cfcf7195467699b0ca
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 17:10:45 +0600

    add text in biryani-shopData file

--- data/biryani-shopData.js
+++ data/biryani-shopData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01319926473",
     wordNo: "৪",
     location: "দেলপাড়া কলেজ রোড সংলগ্ন",
-    detail: "মোরগ পোলাও, ভুনা খিচুড়ি, গরুর কাচ্চি, গরুর বিরিয়ানি।",
+    detail: "মোরগ পোলাও, ভুনা খিচুড়ি, গরুর কাচ্চি ও গরুর বিরিয়ানি পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01623511197",
     wordNo: "৫",
     location: "আমতলা মোড়",
-    detail: "সকালের নাশতা, দুপুরের খাবার ও রাতে মুরগীর গ্রিল, হালিম, বিরিয়ানি, নান রুটি পাওয়া যায়",
+    detail: "সকালের নাশতা, দুপুরের খাবার ও রাতে মুরগীর গ্রিল, হালিম, বিরিয়ানি, নান রুটি পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -28,7 +28,7 @@ const userData = [
     number: "01683128172",
     wordNo: "৫",
     location: "নয়ামাটি দেলপাড়া রোড, নয়ামাটি ডাল সংলগ্ন",
-    detail: "সুস্বাদু বিরিয়ানি, ভুনা খিচুরি, মোরগ পোলাউ পাওয়া যায়। বিভিন্ন অনুষ্ঠানের জন্য অর্ডার সরবরাহ করা হয়।",
+    detail: "সুস্বাদু বিরিয়ানি, ভুনা খিচুরি, মোরগ পোলাউ পাওয়া যায়। এবং বিভিন্ন অনুষ্ঠানের জন্য অর্ডার সরবরাহ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
