5c9eca95572d273a76f58a8f65f492a8474a1c16
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 22:30:40 +0600

    text change in author-of-the-documentData file

--- data/author-of-the-documentData.js
+++ data/author-of-the-documentData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01715451258, 01671772881",
     wordNo: "৫",
     location: "আমতলা মোড়, শাহী মসজিদ সংলগ্ন",
-    detail: "গণপ্রজাতন্ত্রী বাংলাদেশ সরকার কর্তৃক সনদপ্রাপ্ত। এখানে সকল প্রকার দলিল মুসাবিদা ও রেজিষ্টী করা হয় নামজারী, পর্চা, দলিল তল্লাশি সহ যাবতীয় ভূমি সংক্রান্ত কাজের আইনি পরামর্শ দেওয়া হয়।",
+    detail: "গণপ্রজাতন্ত্রী বাংলাদেশ সরকার কর্তৃক সনদপ্রাপ্ত।<br> এখানে সকল প্রকার দলিল মুসাবিদা ও রেজিষ্টী করা হয়। নামজারী, পর্চা, দলিল তল্লাশি সহ যাবতীয় ভূমি সংক্রান্ত কাজের আইনি পরামর্শ দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01766815620",
     wordNo: "৪",
     location: "আদর্শ নগর",
-    detail: "Details about this service",
+    detail: "এখানে সকল প্রকার দলিল মুসাবিদা ও রেজিষ্টী করা হয়। এবং সকল আইনি কাজের পরামর্শ দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
