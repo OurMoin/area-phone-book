78ed6974c6fdf8ad9f34482dcd9a8cf915b36c26
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-16 19:15:58 +0600

    add text in cosmetics-shoData file

--- data/cosmetics-shopData.js
+++ data/cosmetics-shopData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01610717908",
     wordNo: "৫",
     location: "উত্তর শাহী মহল্লা",
-    detail: "বিউটি পার্লার ও কসমেটিক্স",
+    detail: "বিউটি পার্লার ও কসমেটিক্স এর দোকান।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01676037240",
     wordNo: "৫",
     location: "ওয়ায়েস করনী আদর্শ নগর",
-    detail: "বিউটি পার্লার এন্ড কসমেটিক সেন্টার",
+    detail: "বিউটি পার্লার এন্ড কসমেটিক্স এর দোকান।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
