b23dba9b6dcd75349abc35b6f6391f8642e12eac
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 12:38:39 +0600

    photo add in agent-bankData file

--- data/agent-bankData.js
+++ data/agent-bankData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01736767076",
     wordNo: "৪",
     location: "হাজী আহসান উদ্দিন সুপার মার্কেট, ২য় তলা, আদর্শ নগর",
-    detail: "ইসলামী ব্যাংকের সকল সেবা দেওয়া হয়",
+    detail: "ইসলামী ব্যাংকের সকল সেবা দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,29 +17,29 @@ const userData = [
     number: "01917304037",
     wordNo: "৫",
     location: "আমতলা মোড়",
-    detail: "খোলা থাকেঃ<br>২৪/৭ খোলা থাকে",
+    detail: "খোলা থাকেঃ<br>২৪/৭ খোলা থাকে।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F109240667279751%2Fvideos%2F369014363985895%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 3,
     photo: "../cdn/agent-bank/agent-bank.png",
-    name: "IFIC Bank Agent",
+    name: "IFIC Agent Bank",
     number: "Hidden",
-    wordNo: "৫",
+    wordNo: "৪",
     location: "দেলপাড়া কলেজ রোড সংলগ্ন",
-    detail: "IFIC Bank এর সকল সেবা পাওয়া যায়",
+    detail: "IFIC Bank এর সকল সেবা পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 4,
-    photo: "../cdn/agent-bank/agent-bank.png",
+    photo: "../cdn/agent-bank/al-arafah-islamic-bank.jpg",
     name: "আল-আরাফাহ ইসলামী ব্যাংক",
     number: "01408340381",
-    wordNo: "৫",
+    wordNo: "৪",
     location: "দেলপাড়া কলেজ রোড সংলগ্ন",
-    detail: "আল-আরাফাহ ইসলামী ব্যাংক এর সকল সেবা পাওয়া যায়",
+    detail: "আল-আরাফাহ ইসলামী ব্যাংক এর সকল সেবা পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F109240667279751%2Fvideos%2F369014363985895%2F&width=500&show_text=false&height=280&appId"
   }
