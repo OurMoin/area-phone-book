c0ebd5aa79526675fa47c05830ea6e390a4ac4ba
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 20:00:19 +0600

    data entry online-marketingData file

---
+++ data/online-marketingData.js
@@ -0,0 +1,25 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/online-marketing/md-asikur-rahman-polash.png",
+    name: "মোঃ আশিকুর রহমান পলাশ",
+    number: "01521216013",
+    wordNo: "৬",
+    location: "বউবাজার",
+    detail: "প্রফেশনাল অনলাইন মার্কেটের।<br>ফেসবুক, দারাজ ও ইভেলি এ দক্ষ।",
+    postLink: "https://www.facebook.com/md.asikurrahman321",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/online-marketing/md-sharif-imtaz.jpg",
+    name: "মোঃ শরীর ইমতাজ",
+    number: "01521216013",
+    wordNo: "৫",
+    location: "শাহী-মহল্লা",
+    detail: "প্রফেশনাল অনলাইন মার্কেটের।<br>ফেইসবুক মার্কেটিং এ দক্ষ।",
+    postLink: "https://www.facebook.com/SharifImtaz",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
+  
