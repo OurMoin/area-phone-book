00c29d21e8ca2e03d02d234c358690c21c064c42
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 13:28:37 +0600

    wardno add in rent-a-carData file

--- data/rent-a-carData.js
+++ data/rent-a-carData.js
@@ -4,9 +4,9 @@ const userData = [
     photo: "../cdn/rent-a-car/rent-a-car.png",
     name: "আব্দুল রব মিয়ার গ্যারেজ",
     number: "01710569645",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "দক্ষিণ নয়ামাটি বাজার সংলগ্ন",
-    detail: "প্রাইভেট কার, হাইস, নোহা, মিনি পিকআপ ভাড়া দেওয়া হয়",
+    detail: "প্রাইভেট কার, হাইস, নোহা, মিনি পিকআপ ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -15,9 +15,9 @@ const userData = [
     photo: "../cdn/rent-a-car/rent-a-car.png",
     name: "মীর রেন্ট-এ-কার",
     number: "01631092669",
-    wordNo: "৫",
+    wordNo: "৬",
     location: "দক্ষিণ নয়ামাটি বাজার সংলগ্ন",
-    detail: "প্রাইভেট ও হাইস গাড়ি ভাড়া দেওয়া হয়",
+    detail: "প্রাইভেট ও হাইস গাড়ি ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -28,7 +28,7 @@ const userData = [
     number: "01952835050",
     wordNo: "৫",
     location: "শাহী বাজার",
-    detail: "হাইস গাড়ি ভাড়া দেওয়া হয়",
+    detail: "হাইস গাড়ি ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -37,8 +37,8 @@ const userData = [
     photo: "../cdn/rent-a-car/rent-a-car.png",
     name: "আব্দুর রব মিয়ার গ্যারেজ",
     number: "01687774539",
-    wordNo: "৫",
-    location: "নয়ামাটি, ভাবি বাজার রোড।",
+    wordNo: "৬",
+    location: "নয়ামাটি, ভাবি বাজার রোড",
     detail: "সকল প্রকার গাড়ি ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
