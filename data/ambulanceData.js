ef5b3ea966dd9754dc35722dbc96b74b1e127559
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 13:05:01 +0600

    data delete in ambulanceData file

--- data/ambulanceData.js
+++ data/ambulanceData.js
@@ -20,39 +20,6 @@ const userData = [
     detail: "সারা বাংলাদেশে এম্বুলেন্স সার্ভিস দিয়ে থাকে।<br> ২৪ ঘণ্টায় নিয়োজিত ও জরুরি প্রয়োজনে অক্সিজেনের ব্যবস্থা, এসি/ নন এসি, লাশবাহি ফ্রিজিং, আইসিও ও সিসিও এম্বুলেন্স পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
-  // {
-  //   id: 3,
-  //   photo: "../cdn/ambulance/ambulance-1.jpg",
-  //   name: "Conscious Ambulance Service",
-  //   number: "01912603094",
-  //   wordNo: "4",
-  //   location: "নয়ামাটি",
-  //   detail: "সার্ভিস টাইম:<br> সকাল ৯ টা - রাত ৯ টা",
-  //   postLink: "http://fb.com/areaPhonebook",
-  //   videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  // },
-  // {
-  //   id: 4,
-  //   photo: "../cdn/ambulance/ambulance-2.jpg",
-  //   name: "Khaled Ambulance Service",
-  //   number: "01933246577",
-  //   wordNo: "5",
-  //   location: "প্যারাডাইস সিটি",
-  //   detail: "সার্ভিস টাইম:<br> সকাল ৯ টা - রাত ৯ টা",
-  //   postLink: "http://fb.com/areaPhonebook",
-  //   videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  // },
-  // {
-  //   id: 5,
-  //   photo: "../cdn/ambulance/ambulance-3.webp",
-  //   name: "Lamyea Ambulance Services",
-  //   number: "01710070011",
-  //   wordNo: "6",
-  //   location: "বউ বাজার",
-  //   detail: "সার্ভিস টাইম:<br> ২৪ ঘন্টা",
-  //   postLink: "http://fb.com/areaPhonebook",
-  //   videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  // }
+  }
   
 ];
