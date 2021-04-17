3c7b2f76489e8806239e8373b0f2eb52a7a5a765
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 21:56:03 +0600

    some text change in graphic-designData file

--- data/graphic-designData.js
+++ data/graphic-designData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01521216013",
     wordNo: "৫",
     location: "শাহী-মহল্লা",
-    detail: "Multimedia Content Developer.<br>Excerpt in photoshop, Adobe Illustrator, Adobe Premiere, Adobe InDesign, After Effects and exparte in printing and press media.",
+    detail: "Multimedia Content Developer.<br>Excerpt in Adobe Photoshop, Adobe Illustrator, Adobe Premiere, Adobe InDesign, After Effects and exparte in printing and press media.",
     postLink: "https://www.facebook.com/SharifImtaz",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,31 +17,42 @@ const userData = [
     number: "01688202988",
     wordNo: "৪",
     location: "নুরবাগ",
-    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br>ফটোশপ ও ইলাস্ট্রেটর- এ দক্ষ।",
+    detail: "Professional Graphics Designer.<br>Excerpt in Adobe Photoshop and Adobe Illustrator.",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
     id: 3,
+    photo: "../cdn/graphic-design/md-jahid-hasan.jpg",
+    name: "মোঃ জাহিদ হাসান",
+    number: "01679038086",
+    wordNo: "৫",
+    location: "শাহীবাজার",
+    detail: "Professional Graphics Designer.Excerpt in Adobe Photoshop, Adobe Illustrator, Printing and Press.",
+    postLink: "https://www.facebook.com/nobita.q.75",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
     photo: "../cdn/graphic-design/omor-faruk.png",
     name: "ওমর ফারুক",
     number: "01407757364",
     wordNo: "৬",
     location: "বউবাজার",
-    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br>ফটোশপ ও ইলাস্ট্রেটর- এ দক্ষ।",
+    detail: "Professional Graphics Designer.<br>Excerpt in Adobe Photoshop and Adobe Illustrator.",
     postLink: "https://www.facebook.com/profile.php?id=100054960154443",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
-    id: 4,
+    id: 5,
     photo: "../cdn/graphic-design/md-kalam.png",
     name: "মোঃ কালাম",
     number: "01685322989",
     wordNo: "৫",
     location: "রসুলপুর",
-    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br> এডবি ফটোশপ- এ দক্ষ।",
+    detail: "Professional Graphics Designer.<br>Excerpt in Adobe Photoshop.",
     postLink: "https://www.facebook.com/sohag.islam.142",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
+  }
 ];
   
