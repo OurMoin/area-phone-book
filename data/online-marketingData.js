b4c3bb64dd40a34194c0f93bd509206a7687ccda
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 21:47:40 +0600

    some text add in online-marketingData file

--- data/online-marketingData.js
+++ data/online-marketingData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01521216013",
     wordNo: "৬",
     location: "বউবাজার",
-    detail: "প্রফেশনাল অনলাইন মার্কেটের।<br>ফেসবুক, দারাজ ও ইভেলি- এ দক্ষ।",
+    detail: "Professional Online Marketer.<br>Skilled:- Facebook, Daraz and Evaly Marketing.",
     postLink: "https://www.facebook.com/md.asikurrahman321",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -17,7 +17,7 @@ const userData = [
     number: "01521216013",
     wordNo: "৫",
     location: "শাহী-মহল্লা",
-    detail: "প্রফেশনাল অনলাইন মার্কেটের।<br>ফেইসবুক মার্কেটিং- এ দক্ষ।",
+    detail: "Professional Online Marketer.<br>Skilled:- Facebook Marketing.",
     postLink: "https://www.facebook.com/SharifImtaz",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
