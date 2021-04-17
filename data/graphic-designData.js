75e05fa1a2814fd7352ee7b712848c188df3dc36
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 21:04:12 +0600

    data entry in graphic-designData file

---
+++ data/graphic-designData.js
@@ -0,0 +1,47 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/graphic-design/md-sharif-imtaz.jpg",
+    name: "মোঃ শরীর ইমতাজ",
+    number: "01521216013",
+    wordNo: "৫",
+    location: "শাহী-মহল্লা",
+    detail: "Multimedia Content Developer.<br>Excerpt in photoshop, Adobe Illustrator, Adobe Premiere, After Effects and exparte in printing and press media.",
+    postLink: "https://www.facebook.com/SharifImtaz",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/graphic-design/mohsin-alam-rabbi.jpg",
+    name: "মহসিন আলম রাব্বি",
+    number: "01688202988",
+    wordNo: "৪",
+    location: "নুরবাগ",
+    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br>ফটোশপ ও ইলাস্ট্রেটর- এ দক্ষ।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/graphic-design/omor-faruk.png",
+    name: "ওমর ফারুক",
+    number: "01407757364",
+    wordNo: "৬",
+    location: "বউবাজার",
+    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br>ফটোশপ ও ইলাস্ট্রেটর- এ দক্ষ।",
+    postLink: "https://www.facebook.com/profile.php?id=100054960154443",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/graphic-design/md-kalam.png",
+    name: "মোঃ কালাম",
+    number: "01685322989",
+    wordNo: "৬",
+    location: "রসুলপুর",
+    detail: "প্রফেশনাল গ্রাফিক্স ডিজাইনার।<br> এডবি ফটোশপ- এ দক্ষ।",
+    postLink: "https://www.facebook.com/sohag.islam.142",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+];
+  
