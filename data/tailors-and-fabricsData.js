3562b3fe3933987397b1e23ad7e686c0d2c9d388
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 11:57:55 +0600

    data add tailors-and-fabricsData file

--- data/tailors-and-fabricsData.js
+++ data/tailors-and-fabricsData.js
@@ -64,5 +64,16 @@ const userData = [
     detail: "লেডিস এন্ড জেন্টস টেইলার্স",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 7,
+    photo: "../cdn/tailors-and-fabrics/al-medina-burqa-house.jpg",
+    name: "আল-মদিনা বোরকা হাউজ",
+    number: "01748600069",
+    wordNo: "৪",
+    location: "নুরবাগ চৌরাস্তা",
+    detail: "অত্যাধুনিক ডিজাইনের দেশি-বিদেশি বোরকা প্রস্তুতকারক ও পাইকারি বিক্রেতা।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
