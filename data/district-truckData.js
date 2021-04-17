b934ba842337f516b421bf1ca640df5cd4d8a9ad
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-17 14:41:14 +0600

    add text in district-truckData file

--- data/district-truckData.js
+++ data/district-truckData.js
@@ -6,7 +6,7 @@ const userData = [
     number: "01986334181",
     wordNo: "6",
     location: "তাল-তলা, পাগলা",
-    detail: "সকল প্রকার ডিসট্রিক্ট ট্রাক পাওয়া যায়",
+    detail: "সকল প্রকার ডিসট্রিক্ট ট্রাক পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
