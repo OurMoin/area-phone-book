33ec7153951bcd5496d5551a3c44c10a8bb6e101
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-20 19:24:06 +0600

    data add in driving-trainingData file

--- data/driving-trainingData.js
+++ data/driving-trainingData.js
@@ -9,6 +9,17 @@ const userData = [
     detail: "ড্রাইভিং প্রশিক্ষণ দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/driving-training/driving-training.png",
+    name: "আর এফ এম ড্রাইভিং ট্রেনিং সেন্টার এন্ড অটো মোবাইলস",
+    number: "01823758754",
+    wordNo: "সমগ্র পাগলা",
+    location: "পাগলা রেলস্টেশন সংলগ্ন।",
+    detail: "অটো এবং ম্যানুয়াল গাড়ী দ্বারা ড্রাইভিং শিখানো হয়।<br> ড্রাইভিং শিখুন ৩০ দিনে কমপ্লিট কোর্স, ৭০০০ টাকা।<br> ড্রাইভিং কোর্স এর সাথে গাড়ীর ইঞ্জিন, ইলেকট্রিক ও এসির কাজ ফ্রি  শেখানো হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   }
 ];
   
