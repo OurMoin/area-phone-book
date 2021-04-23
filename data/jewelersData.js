014f83a9c0092d1e7dbb3778e24cef3177894013
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-23 13:34:47 +0600

    data add in jewelersData file

--- data/jewelersData.js
+++ data/jewelersData.js
@@ -9,5 +9,16 @@ const userData = [
     detail: "সব ধরনের জুয়েলার্স পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/jewelers/ma-sorno-shilpaloi.jpg",
+    name: "মা-স্বর্ন শিল্পালয়",
+    number: "01611384919",
+    wordNo: "৪",
+    location: "চিতাশাল ঢাল, নুরবাগ রোড",
+    detail: "এখানে আধুনিক ২১/২২ ক্যারেটের গ্যারান্টিযুক্ত খাঁটি স্বনের ও রূপার অলংকার প্রস্তুতকারক ও বিক্রেতা।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
