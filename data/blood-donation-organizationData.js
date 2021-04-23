fb41876f732149d4a4e5ea7324a5dc747f1513d3
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-23 18:44:10 +0600

    data entry in blood-donation-organizationData file

---
+++ data/blood-donation-organizationData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/blood-donation-organization/the-hut-organization.jpg",
+    name: "কুঁড়ে ঘর",
+    number: "01969407116",
+    wordNo: "সমগ্র পাগলা",
+    location: "৬",
+    detail: " কুঁড়েঘর রক্তদান সংগঠন।",
+    postLink: "https://www.facebook.com/groups/190076468489251",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
