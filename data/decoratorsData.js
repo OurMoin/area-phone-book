56e8f012812a694c5022e172689241c5d1be0975
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-23 12:23:31 +0600

    data add in decoratorsData file

--- data/decoratorsData.js
+++ data/decoratorsData.js
@@ -20,5 +20,16 @@ const userData = [
     detail: "বিয়ে এবং অন্যান্য অনুষ্ঠানের প্যান্ডেল ও স্টেজ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/decorators/ma-babar-doya-decorators.jpg",
+    name: "মা-বাবার দোয়া ডেকোরেটর",
+    number: "01813663955",
+    wordNo: "৪",
+    location: "ইকরা স্কুল সংলগ্ন, চিতাশাল",
+    detail: "এখানে ডেকোরেটর এর যাবতীয় মালামাল ও আলোকসজ্জা, জেনারেটর, সাউন্ড সিস্টেম, মাইক, ফ্যান, ডিজে লাইট ভাড়া দেওয়া হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
