e9e3b7c9514c33b9a385fc3a8bdb8c79bb1e16f0
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-24 11:20:59 +0600

    text add in  blood-donation-organizationData file

--- data/blood-donation-organizationData.js
+++ data/blood-donation-organizationData.js
@@ -3,10 +3,10 @@ const userData = [
     id: 1,
     photo: "../cdn/blood-donation-organization/the-hut-organization.jpg",
     name: "কুঁড়ে ঘর",
-    number: "01969407116",
-    wordNo: "সমগ্র পাগলা",
-    location: "৬",
-    detail: " কুঁড়েঘর রক্তদান সংগঠন।",
+    number: "01628010937",
+    wordNo: "৬",
+    location: "রসুলপুর",
+    detail: "কুঁড়েঘর একটি রক্তদান ও সমাজসেবামূলক সংগঠন।<br> হানিফ আহমেদ শান্ত (01628010937)<br> হৃদয় আহমেদ (01969407116)",
     postLink: "https://www.facebook.com/groups/190076468489251",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
