605f4d63a5e94c6d5897a5fa843acdd5f0414536
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-24 12:05:22 +0600

    data entry in blood-donorData file

---
+++ data/blood-donorData.js
@@ -0,0 +1,25 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/blood-donor/the-hut-organization.jpg",
+    name: "হানিফ আহমেদ শান্ত",
+    number: "01628010937",
+    wordNo: "৬",
+    location: "রসুলপুর",
+    detail: "যে কোন ধরনের রক্ত প্রয়োজনের জন্য আমাদের সাথে যোগাযোগ করুন।",
+    postLink: "https://www.facebook.com/hanif.ahmed99",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/blood-donor/the-hut-organization.jpg",
+    name: "হৃদয় আহমেদ",
+    number: "01969407116",
+    wordNo: "৬",
+    location: "রসুলপুর",
+    detail: "যে কোন ধরনের রক্ত প্রয়োজনের জন্য আমাদের সাথে যোগাযোগ করুন।",
+    postLink: "https://www.facebook.com/hridoyahmed5629",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
+  
