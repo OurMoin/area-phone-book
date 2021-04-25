09c6fb10b76505b55e3ed1698ab1c9432eafeae1
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-04-25 20:41:22 +0600

    data add blood-donation-organizationData file

--- data/blood-donation-organizationData.js
+++ data/blood-donation-organizationData.js
@@ -1,6 +1,17 @@
 const userData = [
   {
     id: 1,
+    photo: "../cdn/blood-donation-organization/sanghabaddha-blood-donation-foundation.png",
+    name: "সংঘবদ্ধ রক্তদান ফাউন্ডেশন",
+    number: "01943333810",
+    wordNo: "৬",
+    location: "দক্ষিণ নয়ামাটি",
+    detail: "সংঘবদ্ধ রক্তদান ফাউন্ডেশন একটি রক্ত দান সংগঠন।",
+    postLink: "https://www.facebook.com/groups/2236683726360862",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
     photo: "../cdn/blood-donation-organization/the-hut-organization.jpg",
     name: "কুঁড়ে ঘর",
     number: "01628010937",
@@ -9,5 +20,5 @@ const userData = [
     detail: "কুঁড়েঘর একটি রক্তদান ও সমাজসেবামূলক সংগঠন।<br> হানিফ আহমেদ শান্ত (01628010937)<br> হৃদয় আহমেদ (01969407116)",
     postLink: "https://www.facebook.com/groups/190076468489251",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  }
+  }  
 ];
