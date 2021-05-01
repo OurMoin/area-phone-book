9eeae8e036e8628a5a94d4dc4f456c7c6538dff2
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-01 10:20:53 +0600

    some text remove

--- data/wholesale-medicineData.js
+++ data/wholesale-medicineData.js
@@ -42,16 +42,5 @@ const userData = [
     detail: "সকল প্রকারের ঔষধ পাওয়া যায় এবং পাইকারি বিক্রি করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
-  {
-    id: 5,
-    photo: "../cdn/wholesale-medicine/abdullah-medical-hall.jpg",
-    name: "আব্দুল্লাহ মেডিকেল হল",
-    number: "01717833419",
-    wordNo: "৫",
-    location: "নয়ামাটি মুসলিম পাড়া",
-    detail: "সকল প্রকারের ঔষধ পাওয়া যায় এবং পাইকারি বিক্রি করা হয়।",
-    postLink: "http://fb.com/areaPhonebook",
-    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
