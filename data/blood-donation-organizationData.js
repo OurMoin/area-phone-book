bc49a25b2b4633a9296d4c7ef4aed2955ce09b8b
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-02 19:41:00 +0600

    data add in blood-donation-organizationData file

--- data/blood-donation-organizationData.js
+++ data/blood-donation-organizationData.js
@@ -20,5 +20,16 @@ const userData = [
     detail: "কুঁড়েঘর একটি রক্তদান ও সমাজসেবামূলক সংগঠন।<br> হানিফ আহমেদ শান্ত (01628010937)<br> হৃদয় আহমেদ (01969407116)",
     postLink: "https://www.facebook.com/groups/190076468489251",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  }  
+  },
+  {
+    id: 3,
+    photo: "../cdn/blood-donation-organization/rokter-sondhan.jpg",
+    name: "রক্তের সন্ধান",
+    number: "01537571058",
+    wordNo: "৪",
+    location: "নুরবাগ",
+    detail: "রক্তের সন্ধান একটি রক্তদান সংগঠন।",
+    postLink: "https://www.facebook.com/faysal.ahmedfahim.921",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
 ];
