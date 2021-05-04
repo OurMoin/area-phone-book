082c3c41f6606fe05e9d293a06edcb7411532003
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-04 10:29:44 +0600

    data entry in electric-mechanicData file

---
+++ data/electric-mechanicData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/electric-mechanic/md-delwar.jpg",
+    name: "মোঃ দেলোয়ার",
+    number: "01944118485",
+    wordNo: "৫",
+    location: "শাহী-মহল্লা",
+    detail: "বাড়ি-ঘর ও অফিসের ইলেকট্রিক এর সকল কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
