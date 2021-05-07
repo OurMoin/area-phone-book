d81f8a8b8fa26331d0a1849ae10919a11dbe84bc
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-07 22:23:07 +0600

    data entry in chairman-memberData file

---
+++ data/chairman-memberData.js
@@ -0,0 +1,68 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/chairman-member/haji-md-abu-hanifa.png",
+    name: "হাজী মোঃ আবু হানিফা",
+    number: "01924045053",
+    wordNo: "সমগ্র কুতুবপুর",
+    location: "কুতুবপুর ইউনিয়ন",
+    detail: "কুতুবপুর ইউনিয়ন এর সচিব।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/chairman-member/alhaj-md-monirul-alam-sentu.jpg",
+    name: "হাজী মোঃ আবু হানিফা",
+    number: "01715328866",
+    wordNo: "সমগ্র কুতুবপুর",
+    location: "কুতুবপুর",
+    detail: "ইউপি চেয়ারম্যান কুতুবপুর ইউনিয়ন।",
+    postLink: "https://www.facebook.com/%E0%A6%9C%E0%A6%A8%E0%A6%A4%E0%A6%BE%E0%A6%B0-%E0%A6%9A%E0%A7%87%E0%A7%9F%E0%A6%BE%E0%A6%B0%E0%A6%AE%E0%A7%8D%E0%A6%AF%E0%A6%BE%E0%A6%A8-%E0%A6%86%E0%A6%B2%E0%A6%B9%E0%A6%BE%E0%A6%9C%E0%A7%8D%E0%A6%AC-%E0%A6%AE%E0%A7%8B%E0%A6%83-%E0%A6%AE%E0%A6%A8%E0%A6%BF%E0%A6%B0%E0%A7%81%E0%A6%B2-%E0%A6%86%E0%A6%B2%E0%A6%AE-%E0%A6%B8%E0%A7%87%E0%A6%A8%E0%A7%8D%E0%A6%9F%E0%A7%81-108659677989735/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/chairman-member/anamika-haque.png",
+    name: "অনামিকা হক",
+    number: "01960670663",
+    wordNo: "৪, ৫, ৬",
+    location: "শাহীমহল্লা",
+    detail: "কুতুবপুর ইউনিয়ন পরিষদের মেম্বার। ওয়ার্ড নং:- ৪, ৫, ৬",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/chairman-member/md-zaman.png",
+    name: "মোঃ জামান",
+    number: "01718002093",
+    wordNo: "৪",
+    location: "দেলপাড়া",
+    detail: "কুতুবপুর ইউনিয়ন পরিষদের মেম্বার। ওয়ার্ড নং:- ৪",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 5,
+    photo: "../cdn/chairman-member/haji-md-alauddin-hawladar.png",
+    name: "হাজী মোঃ আলাউদ্দিন হাওলাদার",
+    number: "01715475731",
+    wordNo: "৫",
+    location: "মুসলিমপাড়া",
+    detail: "কুতুবপুর ইউনিয়ন পরিষদের মেম্বার। ওয়ার্ড নং:- ৫",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/chairman-member/haji-md-rokon.png",
+    name: "হাজী মোঃ রোকন",
+    number: "01919504988",
+    wordNo: "৬",
+    location: "বউবাজার",
+    detail: "কুতুবপুর ইউনিয়ন পরিষদের মেম্বার। ওয়ার্ড নং:- ৬",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
