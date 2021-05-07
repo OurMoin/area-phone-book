2114eae0cf96e38d8c6d35d0ed6190fd44a823f3
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-07 22:25:58 +0600

    data entry in union-officeData file

---
+++ data/union-officeData.js
@@ -0,0 +1,13 @@
+const userData = [
+  {
+    id: 1,
+    photo: "../cdn/union-office/haji-md-abu-hanifa.png",
+    name: "হাজী মোঃ আবু হানিফা",
+    number: "01924045053",
+    wordNo: "সমগ্র কুতুবপুর",
+    location: "কুতুবপুর ইউনিয়ন",
+    detail: "kutubpurup@gmail.com <br>কুতুবপুর ইউনিয়ন এর সচিব।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  }
+];
