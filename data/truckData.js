eef9039824a2b8752f833cb1935855df3d12ffeb
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-08 10:20:12 +0600

    data add in truckData file

--- data/truckData.js
+++ data/truckData.js
@@ -9,5 +9,16 @@ const userData = [
     detail: "লোকাল ট্রাক ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 1,
+    photo: "../cdn/truck/truck.png",
+    name: "অনিক ট্রাভেল",
+    number: "01986334181",
+    wordNo: "6",
+    location: "তাল-তলা, পাগলা",
+    detail: "সকল প্রকার ডিসট্রিক্ট ট্রাক পাওয়া যায়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
