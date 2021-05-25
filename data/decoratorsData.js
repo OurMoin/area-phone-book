cee3ed74599ab99a07be58ae4aab396ab560c211
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-05-25 20:59:16 +0600

    data add in decoratorsData file

--- data/decoratorsData.js
+++ data/decoratorsData.js
@@ -31,5 +31,16 @@ const userData = [
     detail: "এখানে ডেকোরেটর এর যাবতীয় মালামাল ও আলোকসজ্জা, জেনারেটর, সাউন্ড সিস্টেম, মাইক, ফ্যান, ডিজে লাইট ভাড়া দেওয়া হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/decorators/royal-decor.jpg",
+    name: "রয়্যাল ডেকর",
+    number: "01679511663",
+    wordNo: "৬",
+    location: "জসীম মার্কেট, মধ্য রসুলপুর",
+    detail: "royaldecor004@gmail.com<br> বিয়ে, বৌ-ভাত, রিসিপশন, জন্মদিন ও পার্টি সহ যেকোন অনুষ্ঠানকে আরো বর্নিল রুপ প্রদানে, আপনার স্মৃতিময় দিনকে আরো স্মরণীয় করে রাখতে পাশে আছে 'রয়্যাল ডেকর'। রুচিসম্মত ও বাহারি সজ্জায় সজ্জিত হোক আপনার চারপাশ। <br>একটি ডেকোরেশন সেট এ পাচ্ছেনঃ-<br> Stage Backdrop.<br> Stage Sofa/Divan.<br> Stage Carpet.<br> Entry Gate with 'Welcome Banner'. <br> Photobooth.<br> Floral walkway with lightning.<br> 20feet carpet for walkway.<br> Leaf ient with lightning and chandelier.",
+    postLink: "https://www.facebook.com/Royal-Decor-103636658571037/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
