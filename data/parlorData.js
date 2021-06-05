be722c1202c1146604825a2648b9a0b4d746b094
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-06-05 20:33:53 +0600

    data add in parlorData file

--- data/parlorData.js
+++ data/parlorData.js
@@ -53,5 +53,16 @@ const userData = [
     detail: "সকল প্রকার পার্লারের কাজ করানো হয়।",
     postLink: "https://www.facebook.com/Delpara-Friends-Gents-Parlour-106197811645183",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/parlor/saaj-beauty-parlor-and-cosmetics.jpg",
+    name: "সাজ বিউটি পার্লার এন্ড কসমেটিক্স",
+    number: "01941938236",
+    wordNo: "৫",
+    location: "শরিফবাগ",
+    detail: "এখানে পার্লারের সকল প্রকার কাজ করা হয়।",
+    postLink: "https://www.facebook.com/saz.beauty.parlour.and.cosmetics/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
