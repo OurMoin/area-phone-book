7df821f7957da0a1160625d3c56818d9d434c628
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-07-17 17:18:32 +0600

    data add in wholesale-storeData

--- data/dealer-pointsData.js
+++ data/dealer-pointsData.js
@@ -9,5 +9,49 @@ const userData = [
     detail: "বসুন্ধরা এর সয়াবিন তেল, আটা, ময়দা, সুজি এবং মেঘনা গ্রুপ এর দুধ, পানি, আটা, ময়দা, সুজি পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "ACI, মমতাজ",
+    number: "01961695060",
+    wordNo: "৬",
+    location: "এস এম মার্কেট, পাগলা",
+    detail: "ACI, এরোসল / রুম স্প, মমতাজ মেহেদীর সকল পণ্য এর ডিলার পয়েন্ট।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "SMC,জয়া",
+    number: "01961695060",
+    wordNo: "৬",
+    location: "এস এম মার্কেট, পাগলা",
+    detail: "SMC টেস্ট  মি / ওর সেলাইন / কন্ডম/ সকল প্রোডাক্ট এর ডিলার পয়েন্ট। জয়া মেপকিন মেডি প্লাস-পেস্ট Colgate-এর সকল প্রোডাক্ট এর ডিলার পয়েন্ট।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "কুমারিকা, কোকোলা",
+    number: "01961695060",
+    wordNo: "৬",
+    location: "এস এম মার্কেট, পাগলা",
+    detail: "কুমারিকা তেল, কোকোলা নুডুলস, আড়ং দুধ, ইউনিভারসাল স্যালাইন, ট্রেইট ব্লেট - ব্লেট এর সকল প্রোডাক্ট এর ডিলার পয়েন্ট।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/wholesale-store/wholesale-store.png",
+    name: "উনিভারসেল, ট্রিট ব্লেড",
+    number: "01961695060",
+    wordNo: "৬",
+    location: "এস এম মার্কেট, পাগলা",
+    detail: "ইউনিভারসাল - স্যালাইন, ট্রেইট ব্লেট - ব্লেট, কুমারিকা তেল, কোকোলা নুডুলস আড়ং দুধ এর সকল প্রোডাক্ট এর ডিলার পয়েন্ট।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
