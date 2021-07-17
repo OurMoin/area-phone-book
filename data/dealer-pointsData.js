fc60d5024d7094826c50e4da13ed154cf0a83aad
Shanto Mondol <52619179+shantomondol@users.noreply.github.com>
2021-07-17 17:21:00 +0600

    data corretion in wholesale-store

--- data/dealer-pointsData.js
+++ data/dealer-pointsData.js
@@ -2,11 +2,11 @@ const userData = [
   {
     id: 1,
     photo: "../cdn/wholesale-store/wholesale-store.png",
-    name: "মিথুন এন্টারপ্রাইজ",
+    name: "বসুন্ধরা ও মেঘনা গ্রুপ",
     number: "01979126418",
     wordNo: "৬",
     location: "নয়ামাটি বাজার",
-    detail: "বসুন্ধরা এর সয়াবিন তেল, আটা, ময়দা, সুজি এবং মেঘনা গ্রুপ এর দুধ, পানি, আটা, ময়দা, সুজি পাওয়া যায়।",
+    detail: "মিথুন এন্টারপ্রাইজ<br> বসুন্ধরা এর সয়াবিন তেল, আটা, ময়দা, সুজি এবং মেঘনা গ্রুপ এর দুধ, পানি, আটা, ময়দা, সুজি পাওয়া যায়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
