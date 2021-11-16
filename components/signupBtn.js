9e283c09732c112713d03faeb5534a77cd5d725a
Moin <ourmoin@gmail.com>
2021-11-16 12:58:25 +0600

    Update signupBtn.js

--- components/signupBtn.js
+++ components/signupBtn.js
@@ -5,7 +5,7 @@ const signupBtnData = [
 function signupBtnTemplate(signupBtn) {
     return `
     <div class="hero-btn">
-        <a class="btn btn-outline-success" href="https://forms.gle/QNqDBmJFR9tnPvwBA" target="_blank"><i
+        <a class="btn btn-outline-success" href="https://forms.gle/5BJxEW7oUE2tRkJN9" target="_blank"><i
                 class="fas fa-user-plus"></i> আপনার তথ্য যোগ করুন</a>
     </div>
   `;
