fc886f1b734595dff2347da344b2c6d8739bd801
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-11-21 19:35:00 +0600

    data add in salonData file

--- data/salonData.js
+++ data/salonData.js
@@ -9,6 +9,17 @@ const userData = [
     detail: "সেলুনের যাবতীয় সকল কাজ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/salon/young-star-gents-parlor-&-ac-salon.jpg",
+    name: "ইয়াং স্টার জেন্টস পার্লার & এসি সেলুন",
+    number: "01635242481",
+    wordNo: "৬",
+    location: "পাগলা বাজার জামে মসজিদের সিড়ির পাশে",
+    detail: "০১৮২০৮২৮০৮১<br> ফেসিয়াল, হেয়ার কালার, হেয়ার ষ্টেইট এবং রিবন্ডিং।<br> চুল কাটা- ৮০ টাকা<br> ফোম সেভ- ৭০ টাকা<br> জেল সেভ-৮০টাকা",
+    postLink: "https://www.facebook.com/groups/371332293457017/permalink/915384312385143/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
   
