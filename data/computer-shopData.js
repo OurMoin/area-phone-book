bff7ae996589952515105ef01f74045379fe6e42
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-11-21 19:16:53 +0600

    data add in computer-shopData file

---
+++ data/computer-shopData.js
@@ -0,0 +1,14 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/computer-shop/sr-computer-center.jpg",
+      name: "SR Computer Center",
+      number: "01518314865",
+      wordNo: "৬",
+      location: "দোকান নং- ২১, কাজী খোরশেদ প্লাজা পাগলা",
+      detail: "Proprietor : Md. Sajjat Hossain Shawon<br> কম্পিউটার কম্পোজ, ফটোকপি, অনলাইন আবেদন, ছবি থেকে ছবি সহ যাবতীয় কাজ করা হয়।",
+      postLink: "http://fb.com/areaPhonebook",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
