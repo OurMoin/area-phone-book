947d6b3bf393ee35941d4b1d943d9b724dbf34b3
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-11-21 20:54:08 +0600

    data entry in foodData file

---
+++ data/foodData.js
@@ -0,0 +1,14 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/food/monika's-mannequin.png",
+      name: "Monika's Mannequin",
+      number: "01533493999",
+      wordNo: "৫",
+      location: "শাহীবাজার, আমতলা",
+      detail: "অনলাইন শপিং(লেডিস টি-শার্ট, ফ্রী-পিস, হিজাব. জেন্টস প্যান্ট, পাঞ্জাবি, টি-শার্ট এন্ড হ্যান্ড পেইন্ট আইটেম) পাওয়া যায়।",
+      postLink: "https://www.facebook.com/monikasmannequin",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
