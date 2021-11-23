ef2ab24d1336e888ebf04be74c17f965d5788f6d
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-11-23 19:29:01 +0600

    change icon

--- components/emergency-services-categoryData.js
+++ components/emergency-services-categoryData.js
@@ -1,43 +1,43 @@
 const categoryData = [
     {
       id: 1,
-      photo: "../resources/icons/emergency-services/ambulance.jpg",
+      photo: "../resources/icons/emergency-services/ambulance.png",
       name: "এ্যাম্বুলেন্স",
       link: "ambulance.html"
     },
     {
       id: 2,
-      photo: "../resources/icons/emergency-services/hospital.jpg",
+      photo: "../resources/icons/emergency-services/hospital.png",
       name: "হাঁসপাতাল",
       link: "hospital.html"
     },
     {
       id: 3,
-      photo: "../resources/icons/emergency-services/electricity-office.jpg",
+      photo: "../resources/icons/emergency-services/electricity-office.png",
       name: "বিদ্যুৎ অফিস",
       link: "electricity-office.html"
     },
     {
       id: 4,
-      photo: "../resources/icons/emergency-services/police-station.jpg",
+      photo: "../resources/icons/emergency-services/police-station.png",
       name: "পুলিশ স্টেশন",
       link: "police-station.html"
     },
     {
       id: 5,
-      photo: "../resources/icons/emergency-services/fire-service.jpg",
+      photo: "../resources/icons/emergency-services/fire-service.png",
       name: "ফায়ার সার্ভিস",
       link: "fire-service.html"
     },
     {
       id: 6,
-      photo: "../resources/icons/emergency-services/car-rental.jpg",
+      photo: "../resources/icons/emergency-services/car-rental.png",
       name: "গাড়ি ভাড়া",
       link: "car-rental.html"
     },
     {
       id: 7,
-      photo: "../resources/icons/emergency-services/government-notice.jpg",
+      photo: "../resources/icons/emergency-services/government-notice.png",
       name: "সরকারি নোটিশ",
       link: "government-notice.html"
     }
