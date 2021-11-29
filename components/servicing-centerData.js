7ab38faf3852a52b87c1ec0a187e2a97b397470a
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-11-29 21:35:49 +0600

    add servicing-center page

---
+++ components/servicing-centerData.js
@@ -0,0 +1,51 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/medical-services/pharmacy.png",
+      name: "মোবাইল সার্ভিসিং",
+      link: "pharmacy"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/medical-services/wholesale-medicine.png",
+      name: "এসি / ফ্রিজ সার্ভিসিং",
+      link: "wholesale-medicine"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/medical-services/hospital.png",
+      name: "কম্পিউটার সার্ভিসিং",
+      link: "hospital"
+    },
+    {
+      id: 4,
+      photo: "../resources/icons/medical-services/doctor-sat-down.png",
+      name: "টিভি সার্ভিসিং",
+      link: "doctor-sat-down"
+    },
+    {
+      id: 5,
+      photo: "../resources/icons/medical-services/doctor-lab.png",
+      name: "ইলেক্টিক সার্ভিসিং",
+      link: "doctor-lab"
+    },
+    {
+      id: 6,
+      photo: "../resources/icons/medical-services/homeopathy.png",
+      name: "সাইকেল সার্ভিসিং",
+      link: "homeopathy"
+    },
+    {
+      id: 7,
+      photo: "../resources/icons/medical-services/physiotherapy.png",
+      name: "বাইক সার্ভিসিং",
+      link: "physiotherapy"
+    },
+    {
+      id: 8,
+      photo: "../resources/icons/medical-services/hijama.png",
+      name: "গাড়ি সার্ভিসিং",
+      link: "hijama"
+    }
+  ];
+  
