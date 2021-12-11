23197bc0a2c25d6824622a3a07fda5bef417917a
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-11 18:16:14 +0600

    name change

--- components/training-centerData.js
+++ components/training-centerData.js
@@ -1,61 +1,61 @@
 const categoryData = [
     {
       id: 1,
-      photo: "../resources/icons/education-services/computer-training.png",
+      photo: "../resources/icons/training-center/computer-training.png",
       name: "কম্পিউটার ট্রেনিং",
       link: "computer-training"
     },
     {
       id: 2,
-      photo: "../resources/icons/education-services/driving-training.png",
+      photo: "../resources/icons/training-center/driving-training.png",
       name: "ড্রাইভিং ট্রেনিং",
       link: "driving-training"
     },
     {
       id: 3,
-      photo: "../resources/icons/education-services/language-training.png",
+      photo: "../resources/icons/training-center/language-training.png",
       name: "ভাষা ট্রেনিং",
       link: "language-training"
     },
     {
       id: 4,
-      photo: "../resources/icons/education-services/skill-training.png",
+      photo: "../resources/icons/training-center/skill-training.png",
       name: "স্কিল ট্রেনিং",
       link: "skill-training"
     },
     {
       id: 5,
-      photo: "../resources/icons/education-services/electrical-training.png",
+      photo: "../resources/icons/training-center/electrical-training.png",
       name: "ইলেকট্রিক ট্রেনিং",
       link: "electrical-training"
     },
     {
       id: 6,
-      photo: "../resources/icons/education-services/workshop-training.png",
+      photo: "../resources/icons/training-center/workshop-training.png",
       name: "ওয়ার্কশপ ট্রেনিং",
       link: "workshop-training"
     },
     {
       id: 7,
-      photo: "../resources/icons/education-services/trailing-training.png",
+      photo: "../resources/icons/training-center/trailing-training.png",
       name: "টেইলারিং ট্রেনিং",
       link: "trailing-training"
     },
     {
       id: 8,
-      photo: "../resources/icons/education-services/ac-fridge-servicing-training.png",
+      photo: "../resources/icons/training-center/ac-fridge-servicing-training.png",
       name: "এসি/ফ্রিজ সার্ভিসিং ট্রেনিং",
       link: "ac-fridge-servicing-training"
     },
     {
       id: 9,
-      photo: "../resources/icons/education-services/parlor-training.png",
+      photo: "../resources/icons/training-center/parlor-training.png",
       name: "পার্লার ট্রেনিং",
       link: "parlor-training"
     },
     {
       id: 10,
-      photo: "../resources/icons/education-services/gym.png",
+      photo: "../resources/icons/training-center/gym.png",
       name: "Gym",
       link: "gym"
     }
