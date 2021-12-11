47d57f15eb1e4dc9b1cf3fc60b2bfc14591994c6
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-11 18:34:16 +0600

    extra file remove

--- components/education-servicesData.js
+++ components/education-servicesData.js
@@ -3,43 +3,43 @@ const categoryData = [
       id: 1,
       photo: "../resources/icons/education-services/school.png",
       name: "স্কুল",
-      link: "school."
+      link: "school"
     },
     {
       id: 2,
       photo: "../resources/icons/education-services/college.png",
       name: "কলেজ",
-      link: "college."
+      link: "college"
     },
     {
       id: 3,
       photo: "../resources/icons/education-services/madrasa.png",
       name: "মাদ্রাসা",
-      link: "madrasa."
+      link: "madrasa"
     },
     {
       id: 4,
       photo: "../resources/icons/education-services/coaching-center.png",
       name: "কোচিং সেন্টার",
-      link: "coaching-center."
+      link: "coaching-center"
     },
     {
       id: 5,
       photo: "../resources/icons/education-services/home-teacher.png",
       name: "হোম টিচার",
-      link: "home-teacher."
+      link: "home-teacher"
     },
     {
       id: 6,
       photo: "../resources/icons/education-services/library.png",
       name: "লাইব্রেরি",
-      link: "library."
+      link: "library"
     },
     {
       id: 7,
       photo: "../resources/icons/education-services/all-services-of-educational-institutions.png",
       name: "শিক্ষা প্রতিষ্ঠানের সকল সেবা",
-      link: "all-services-of-educational-institutions."
+      link: "all-services-of-educational-institutions"
     }
   ];
   
