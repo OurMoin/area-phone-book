3ef92f39619961c47ee663fe0c984d3948987777
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-22 21:58:16 +0600

    data add and link change in parlor-salon file

---
+++ components/parlor-salonData.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/parlor-salon/parlor.png",
+      name: "পার্লার",
+      link: "parlor"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/parlor-salon/salon.png",
+      name: "সেলুন",
+      link: "salon"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/parlor-salon/cosmetics-shop.png",
+      name: "কসমিটিক্স এর দোকান",
+      link: "cosmetics-shop"
+    }
+  ];
+  
