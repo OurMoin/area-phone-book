5895befc52f2ded463c7b2610236623a807a23e8
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-22 21:41:25 +0600

    data add

---
+++ components/food-corner_food-storeData.js
@@ -0,0 +1,39 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/food-corner_food-store/restaurant.png",
+      name: "রেস্টুরেন্ট",
+      link: "restaurant"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/food-corner_food-store/biryani-shop.png",
+      name: "বিরিয়ানির দোকান",
+      link: "biryani-shop"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/food-corner_food-store/fuchka-shop.png",
+      name: "ফুচকার দোকান",
+      link: "fuchka-shop"
+    },
+    {
+      id: 4,
+      photo: "../resources/icons/food-corner_food-store/fast-food.png",
+      name: "ফাস্ট ফুড",
+      link: "fast-food"
+    },
+    {
+      id: 5,
+      photo: "../resources/icons/food-corner_food-store/cake-house.png",
+      name: "কেক ঘর",
+      link: "cake-house"
+    },
+    {
+      id: 6,
+      photo: "../resources/icons/food-corner_food-store/sweet-shop.png",
+      name: "মিষ্টির দোকান",
+      link: "sweet-shop"
+    }
+  ];
+  
