ccf242ff21d359ef65d01110f6203010a51028f5
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-22 22:19:33 +0600

    data add in mechanic file

---
+++ components/mechanic.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/mechanic/painter.png",
+      name: "রং মিস্ত্রী",
+      link: "painter"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/mechanic/carpenter.png",
+      name: "কাঠ মিস্ত্রী",
+      link: "carpenter"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/mechanic/electric-mechanic.png",
+      name: "ইলেকট্রিক মিস্ত্রী",
+      link: "electric-mechanic"
+    }
+  ];
+  
