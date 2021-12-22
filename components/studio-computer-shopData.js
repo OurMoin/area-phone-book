dade463809004a7b7054ec23e3b727a0243217c1
Shanto <52619179+shantomondol@users.noreply.github.com>
2021-12-22 21:51:20 +0600

    change link and add data

---
+++ components/studio-computer-shopData.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/studio-computer-shop/studio.png",
+      name: "স্টুডিও",
+      link: "studio"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/studio-computer-shop/cameraman.png",
+      name: "ক্যামেরাম্যান",
+      link: "cameraman"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/studio-computer-shop/computer-shop.png",
+      name: "কম্পিউটার দোকান",
+      link: "computer-shop"
+    }
+  ];
+  
