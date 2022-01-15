39f99a3a385754c9f6bd6a9d0523079d0927b51c
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-01-15 22:51:43 +0600

    category page add

---
+++ components/civic-serviceData.js
@@ -0,0 +1,45 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/civic-service/author-of-the-document.png",
+      name: "দলিল লেখক",
+      link: "author-of-the-document"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/civic-service/union-office.png",
+      name: "ইউনিয়ন অফিস",
+      link: "union-office"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/civic-service/chairman-member.png",
+      name: "চেয়ারম্যান-মেম্বার",
+      link: "chairman-member"
+    },
+    {
+      id: 4,
+      photo: "../resources/icons/civic-service/chairman-office.png",
+      name: "চেয়ারম্যান অফিস",
+      link: "chairman-office"
+    },
+    {
+      id: 5,
+      photo: "../resources/icons/civic-service/voter-office.png",
+      name: "ভোটার অফিস",
+      link: "voter-office"
+    },
+    {
+      id: 6,
+      photo: "../resources/icons/civic-service/land-office.png",
+      name: "ভূমি অফিস",
+      link: "land-office"
+    },
+    {
+      id: 7,
+      photo: "../resources/icons/civic-service/kazi-office.png",
+      name: "কাজী অফিস",
+      link: "kazi-office"
+    }
+  ];
+  
