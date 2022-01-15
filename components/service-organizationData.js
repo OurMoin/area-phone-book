1a9932fc6d5f4c029c93566914b274c143ea9fee
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-01-15 23:07:31 +0600

    category page add in social-organization file

---
+++ components/service-organizationData.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/service-organization/social-organization.png",
+      name: "সামাজিক সংগঠন",
+      link: "social-organization"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/service-organization/youth-organizations.png",
+      name: "যুব সংগঠন",
+      link: "youth-organizations"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/service-organization/blood-donation.png",
+      name: "রক্ত দান",
+      link: "blood-donation"
+    }
+  ];
+  
