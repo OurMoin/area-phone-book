35c62f6152d152d853d3ba1b8a796103fc0158c0
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-01-15 23:36:46 +0600

    add category page in online-sellers file

---
+++ components/online-sellersData.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/online-sellers/food.png",
+      name: "ফুড",
+      link: "food"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/online-sellers/clothes.png",
+      name: "কাপড়",
+      link: "clothes"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/online-sellers/cosmetics.png",
+      name: "কসমেটিক্স",
+      link: "cosmetics"
+    }
+  ];
+  
