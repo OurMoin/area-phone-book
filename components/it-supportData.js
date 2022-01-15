fb4644c4e0a4c12f2d4d179c5edaba8bb202c042
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-01-15 23:22:16 +0600

    category page add in it-suppot file

---
+++ components/it-supportData.js
@@ -0,0 +1,39 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/service-organization/online-marketing.png",
+      name: "Online Marketing",
+      link: "online-marketing"
+    },
+    {
+      id: 2,
+      photo: "../resources/icons/service-organization/graphic-design.png",
+      name: "Graphic Design",
+      link: "graphic-design"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/service-organization/web-design.png",
+      name: "Web Design",
+      link: "web-design"
+    },
+    {
+      id: 4,
+      photo: "../resources/icons/service-organization/web-development.png",
+      name: "Web Development",
+      link: "web-development"
+    },
+    {
+      id: 5,
+      photo: "../resources/icons/service-organization/seo.png",
+      name: "SEO",
+      link: "seo"
+    },
+    {
+      id: 6,
+      photo: "../resources/icons/service-organization/apps-developer.png",
+      name: "Apps Developer",
+      link: "apps-developer"
+    }
+  ];
+  
