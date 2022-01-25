3700dd2b39bec8bebe2c341db857c80d074c26e3
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-01-25 12:22:27 +0600

    Update categoryData.js

--- components/categoryData.js
+++ components/categoryData.js
@@ -20,6 +20,26 @@ const categoryData = [
         ]
     },
     {
+      id: 27,
+      photo: "../resources/icons/train-timetable.png",
+      name: "বর্তমান ট্রেনের সময়সূচি",
+      subCategory:
+        [
+          {
+            name: "ঢাকা - নারায়ণগঞ্জ",
+            link: "dhaka-narayanganj"
+          },
+          {
+            name: "পূর্বাঞ্চল",
+            link: "https://railway.portal.gov.bd/site/page/988258c9-5f11-4719-91e2-fbc898d4c2a9"
+          },
+          {
+            name: "পশ্চিমাঞ্চল",
+            link: "https://railway.portal.gov.bd/site/page/6f618d21-8c9a-41da-a896-d5ed83b53b17"
+          }
+        ]
+    },
+    {
       id: 1,
       photo: "../resources/icons/emergency-services.png",
       name: "জরুরী সেবা",
