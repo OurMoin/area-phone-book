383a5cd8fdbbd2885659421b90fa1cd404e19bc3
Moin <ourmoin@gmail.com>
2022-02-11 01:32:48 +0600

    Update signupBtn.js

--- components/signupBtn.js
+++ components/signupBtn.js
@@ -5,7 +5,7 @@ const signupBtnData = [
 function signupBtnTemplate(signupBtn) {
     return `
     <div class="hero-btn">
-        <a class="btn btn-danger" target="_blank" href="https://forms.gle/5BJxEW7oUE2tRkJN9"><i
+        <a class="btn btn-danger" target="_blank" href="https://www.areaphonebook.com/signup"><i
                 class="fas fa-user-plus"></i> আপনার তথ্য যোগ করুন</a>
     </div>
   `;
