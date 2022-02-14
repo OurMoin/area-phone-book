075d23b1eac99e5afb188a1a949b60480d0760ff
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-14 14:55:45 +0600

    chat plugin off

--- data/schoolData.js
+++ data/schoolData.js
@@ -75,6 +75,39 @@ const userData = [
     detail: "প্লে থেকে পঞ্চম শ্রেনী পর্যন্ত শিক্ষা প্রধান করা হয়।<br> শিক্ষাই সত্য, সত্যই অনন্ত সুন্দর।",
     postLink: "https://www.facebook.com/AnirbanAdarshaBiddyapeeth2020",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/school/school.png",
+    name: "কুমিল্লা সমিতি মডেল স্কুল এন্ড কলেজ",
+    number: "01712276629",
+    wordNo: "৫",
+    location: "শাহী মহল্লা",
+    detail: "৩য় শ্রেণী থেকে ৯ম শ্রেণী পর্যন্ত, মানবিক ও ব্যবসায় শিক্ষা শাখায় ভর্তি চলছে।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 9,
+    photo: "../cdn/school/school.png",
+    name: "জুনিয়র স্কলার স্কুল",
+    number: "01675984527",
+    wordNo: "৫",
+    location: "শাহী বাজার রোড নুরবাগ",
+    detail: "প্লে থেকে পঞ্চম শ্রেনী পর্যন্ত শিক্ষা প্রধান করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 10,
+    photo: "../cdn/school/prime-junior-school.jpg",
+    name: "প্রাইম জুনিয়র স্কুল",
+    number: "01712424044",
+    wordNo: "৫",
+    location: "আমতলা, শাহী বাজার",
+    detail: "প্লে থেকে নবম শ্রেণী পর্যন্ত শিক্ষা প্রধান করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
   
