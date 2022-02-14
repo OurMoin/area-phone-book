c76d53ce33b80a93ccbdf534510a3fdef8041d7f
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-14 17:12:58 +0600

    data add in clothesData file

--- data/clothesData.js
+++ data/clothesData.js
@@ -9,6 +9,17 @@ const userData = [
       detail: "অনলাইন শপিং(লেডিস টি-শার্ট, ফ্রী-পিস, হিজাব. জেন্টস প্যান্ট, পাঞ্জাবি, টি-শার্ট এন্ড হ্যান্ড পেইন্ট আইটেম) পাওয়া যায়।",
       postLink: "https://www.facebook.com/monikasmannequin",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/clothes/rj-fashion-mart.jpg",
+      name: "RJ Fashion Mart",
+      number: "01991894967",
+      wordNo: "৫",
+      location: "উত্তর রসূলপুর(পূর্বপাড়া)",
+      detail: "মেয়েদের যাবতীয় পোশাক, জুয়েলারী ও বিউটি প্রোডাক্ট ইত্যাদি পাওয়া যায়।",
+      postLink: "https://www.facebook.com/jrfashionmart7",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
