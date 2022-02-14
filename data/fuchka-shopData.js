be568315f4efda550bed15f44cb0260ae603f872
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-14 17:32:16 +0600

    data add in fuchka-shopData file

---
+++ data/fuchka-shopData.js
@@ -0,0 +1,25 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/fuchka-shop/ruchi-chatpati-fuska-and-halim-house.jpg",
+      name: "রুচি চটপটি, ফুসকা ও হালিম হাউজ",
+      number: "01779884945",
+      wordNo: "৪",
+      location: "চিতাশাল, পাগলা",
+      detail: "এখানে রুচিসম্মত ফুসকা, চটপটি ও হালিম পাওয়া যায়। ",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/fuchka-shop/shimul-fuchka-and-chatputi-house.jpg",
+      name: "শিমুল ফুচকা ও চটপুটি হাউজ",
+      number: "01780809008",
+      wordNo: "৪",
+      location: "চিতাশাল ব্রিজ সংলগ্ন",
+      detail: "এখানে মানসম্মত চটপটি, ফুচকা ও সু-স্বাদু নুডুলস্ পাওয়া যায়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
