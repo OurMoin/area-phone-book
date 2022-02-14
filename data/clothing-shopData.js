6e75b4597991a395fbe7b1e6ba26819964f0d656
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-14 17:52:29 +0600

    data add in clothing-shopData file

--- data/clothing-shopData.js
+++ data/clothing-shopData.js
@@ -20,5 +20,27 @@ const userData = [
     detail: "অত্যাধুনিক ডিজাইনের দেশি-বিদেশি বোরকা প্রস্তুতকারক ও পাইকারি বিক্রেতা।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/clothing-shop/mayer-doya-readymade-corner.jpg",
+    name: "মায়ের দোয়া রেডিমেট কর্ণার",
+    number: "01400437754",
+    wordNo: "৬",
+    location: "পাগলা বাজার, বাদশা প্লাজা, দোকান নং-৭",
+    detail: "এখানে উন্নত মানের দেশী, বিদেশি, ছেলেদের শার্ট, প্যান্ট, পাঞ্জাবি ও গেঞ্জি ইত্যাদি পাওয়া যায়।",
+    postLink: "https://www.facebook.com/mdanisur.rahman.94214",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/clothing-shop/brands-outlet.jpg",
+    name: "Brands Outlet",
+    number: "01876519364",
+    wordNo: "৬",
+    location: "পাগলা বাজার",
+    detail: "এখানে উন্নত মানের ছেলেদের শার্ট, প্যান্ট, পাঞ্জাবি, গেঞ্জি, ঘড়ি ও চশমা ইত্যাদি পাওয়া যায়।",
+    postLink: "https://www.facebook.com/mdanisur.rahman.94214",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
