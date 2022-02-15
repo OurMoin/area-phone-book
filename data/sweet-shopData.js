edededa4a2e8b99a0ce1c078d211db5130e3e823
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-15 10:20:51 +0600

    data add in sweet-shopData file

--- data/sweet-shopData.js
+++ data/sweet-shopData.js
@@ -12,8 +12,8 @@ const userData = [
   },
   {
     id: 2,
-    photo: "../cdn/sweet-shop/haji-food-suites-and-bakery.jpg",
-    name: "হাজী ফুড সুইস্ট এন্ড বেকারি",
+    photo: "../cdn/sweet-shop/haji-bakery-doi-sweet.jpg",
+    name: "হাজী বেকারী দই মিষ্টি",
     number: "01748271952",
     wordNo: "৪",
     location: "দেলপাড়া বাজার, দেলপাড়া",
@@ -29,6 +29,17 @@ const userData = [
     wordNo: "৬",
     location: "পাগলা বাজার",
     detail: "প্রসিদ্ধ দধি, মিষ্টি, রসমালাই, সন্ডেন প্যারা, সন্ডেস ক্ষিরা, লাড্ডু, জর্দা মিষ্টি, ঘি বিক্রেতা ও সরবরাহকারি।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/sweet-shop/haji-food-sweet-and-bakery.jpg",
+    name: "হাজী ফুড সুইস্ট এন্ড বেকারী",
+    number: "01986030787",
+    wordNo: "৫",
+    location: "আদর্শ নগর",
+    detail: "মিষ্টি ও বেকারি এর সকল পন্য।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
