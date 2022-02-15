7c013ebcfed04ff54d4603e8dee06d937dec08b5
moinul-islam <52619179+shantomondol@users.noreply.github.com>
2022-02-15 13:40:33 +0600

    data add in gymData file

--- data/gymData.js
+++ data/gymData.js
@@ -9,6 +9,28 @@ const userData = [
       detail: "GYM Training",
       postLink: "http://fb.com/areaPhonebook",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/gym/hasan-fitness-gym.jpg",
+      name: "Hasan Fitness Gym",
+      number: "01771025000",
+      wordNo: "অন্যান্য",
+      location: "জালকুড়ী, নারায়ণগঞ্জ",
+      detail: "ছেলেদের জন্য খোলা থাকেঃ<br> শনিবার - বৃহস্পতিবার<br> সকাল ৫টা - দুপুর ১টা পর্যন্ত।<br> বিকেল ৫টা - রাত ১১টা পর্যন্ত।<br> শুক্রবার ( বিকেল ৫টা - রাত ৯টা পর্যন্ত। )<br> মেয়েদের জন্য খোলা থাকেঃ<br> শনিবার - বৃহস্পতিবার<br> দুপুর ১টা - বিকেল ৫টা পর্যন্ত।<br> শুক্রবার বন্ধ।",
+      postLink: "http://fb.com/areaPhonebook",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 3,
+      photo: "../cdn/gym/100-muscle-fit.png",
+      name: "100% Muscle Fit",
+      number: "01945556782",
+      wordNo: "অন্যান্য",
+      location: "মাহবুদুল হক খান প্লাজা ৫ম তলা, রায়েরবাগ বাস স্ট্যান্ড",
+      detail: "",
+      postLink: "http://fb.com/areaPhonebook",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
