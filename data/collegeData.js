13451dfaae3cec99d42c45f8ce845f1cc55caa76
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-18 22:00:28 +0600

    data entry in collegeData file

---
+++ data/collegeData.js
@@ -0,0 +1,14 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/college/narayanganj-commerce-college.png",
+      name: "Narayanganj Commerce College - NCC",
+      number: "01817567116",
+      wordNo: "অন্যান্য",
+      location: "৭০ উত্তর চাষাড়া, নারায়ণগঞ্জ",
+      detail: "একাদশ - দ্বাদশ শ্রেণী পর্যন্ত শিক্ষা প্রধান করা হয়।",
+      postLink: "https://www.facebook.com/n.ganj.commerce.college",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
