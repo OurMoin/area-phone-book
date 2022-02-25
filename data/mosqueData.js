3fd62ff3282b403d956bfbea6feb607a73d3d30f
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-25 12:20:24 +0600

    data entry in MosqueData file

---
+++ data/mosqueData.js
@@ -0,0 +1,14 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/Mosque/shahi-madina-jame-mosque.jpg",
+      name: "শাহী মদীনা জামে মসজিদ",
+      number: "01671432099",
+      wordNo: "৫",
+      location: "শাহী বাজার আমতলা",
+      detail: "ধার্মিক বিষয় সকল তথ্য দেয়া হয় ইনশাআল্লাহ নামাজ সহ সকল নূরানী সহীহ সুদ্ধ ভাবে কোরআন পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
