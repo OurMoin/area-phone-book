41ca4871fbbc809280ce41ad9cfbe98e30464b49
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-25 13:35:34 +0600

    data add in mosqueData file

--- data/mosqueData.js
+++ data/mosqueData.js
@@ -9,6 +9,226 @@ const userData = [
       detail: "ধার্মিক বিষয় সকল তথ্য দেয়া হয় ইনশাআল্লাহ নামাজ সহ সকল নূরানী সহীহ সুদ্ধ ভাবে কোরআন পড়ানো হয়।",
       postLink: "https://www.facebook.com/AreaPhonebook/",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/mosque/mosque.png",
+      name: "পাগলা বাজার জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "পাগলা বাজার",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 3,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বায়তুল ফালাহ জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "পাগলা বাজার",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 4,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুল আফিয়া জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "তালতলা",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 5,
+      photo: "../cdn/mosque/mosque.png",
+      name: "তালতলা মাদ্রাসা জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "তালতলা",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 6,
+      photo: "../cdn/mosque/mosque.png",
+      name: "দক্ষিণ নায়ামাটি জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "6",
+      location: "দক্ষিণ নায়ামাটি",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 7,
+      photo: "../cdn/mosque/mosque.png",
+      name: "পশ্চিম নায়ামাটি রেললাইন জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "পশ্চিম নায়ামাটি",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 8,
+      photo: "../cdn/mosque/mosque.png",
+      name: "রাসুল পাক (সঃ) জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "পাগলা উচ্চ বিদ্যালয়",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 9,
+      photo: "../cdn/mosque/mosque.png",
+      name: "প্যারাডাইস সিটি জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "প্যারাডাইস সিটি",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 10,
+      photo: "../cdn/mosque/mosque.png",
+      name: "পাগলা পূর্বপাড়া ইসলামিয়া জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "পাগলা পূর্বপাড়া",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 11,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুল আমান জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "মুসলিমপাড়া",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 12,
+      photo: "../cdn/mosque/mosque.png",
+      name: "নয়ামাটি কেন্দ্রীয় জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "নয়ামাটি",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 13,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুল জান্নাত জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "মুসলিমপাড়া",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 14,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুল নূর জাহান জামে মসজিদ",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "উত্তর রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 15,
+      photo: "../cdn/mosque/mosque.png",
+      name: "ফাতেমা নূর জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 16,
+      photo: "../cdn/mosque/mosque.png",
+      name: "রসুলপুর জামে মসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 17,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুত তোফাজ্জল জমে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 18,
+      photo: "../cdn/mosque/mosque.png",
+      name: "পশ্চিম রসুলপুর জামে মসজিদ",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "পশ্চিম রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 19,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বায়তুস সালাম জামে মসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "দক্ষিন রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 20,
+      photo: "../cdn/mosque/mosque.png",
+      name: "মদিনাতুল জান্নাত জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "পশ্চিম রসুলপুর",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 21,
+      photo: "../cdn/mosque/mosque.png",
+      name: "বাইতুল আমান জামে মাসজিদ",
+      number: "01798899911",
+      wordNo: "৬",
+      location: "বৌ বাজার",
+      detail: "ধার্মিক বিষয় সকল তথ্য ও নামাজ পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
