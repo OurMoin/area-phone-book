61119dc1226b05ba47adcfa90bc783dc2b4692e3
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-28 10:00:45 +0600

    category photo add in national-information-windowData file

--- components/national-information-windowData.js
+++ components/national-information-windowData.js
@@ -2,37 +2,37 @@ const categoryData = [
     
     {
       id: 1,
-      photo: "../resources/icons/ek-nojore-kutubpur/ministry-of-government.png",
+      photo: "../resources/icons/national-information-window/ministry-of-government.png",
       name: "সরকারী মন্ত্রণালয়",
       link: "ministry-of-government"
     },
     {
       id: 2,
-      photo: "../resources/icons/ek-nojore-kutubpur/department.png",
+      photo: "../resources/icons/national-information-window/department.png",
       name: "অধিদপ্তর",
       link: "department"
     },
     {
       id: 3,
-      photo: "../resources/icons/ek-nojore-kutubpur/universities.png",
+      photo: "../resources/icons/national-information-window/universities.png",
       name: "বিশ্ববিদ্যালয় সমূহ",
       link: "universities"
     },
     {
       id: 4,
-      photo: "../resources/icons/ek-nojore-kutubpur/citizenship-certificate.png",
+      photo: "../resources/icons/national-information-window/citizenship-certificate.png",
       name: "নাগরিত্ব সনদ",
       link: "citizenship-certificate"
     },
     {
       id: 5,
-      photo: "../resources/icons/ek-nojore-kutubpur/result.png",
+      photo: "../resources/icons/national-information-window/result.png",
       name: "রেজাল্ট",
       link: "result"
     },
     {
       id: 6,
-      photo: "../resources/icons/ek-nojore-kutubpur/department-of-food.png",
+      photo: "../resources/icons/national-information-window/department-of-food.png",
       name: "খাদ্য অধিদপ্তর",
       link: "department-of-food"
     }
