b921e7576ef55e4c6def484924670049e3f6104c
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-28 15:48:17 +0600

    data shorten

--- data/ophthalmologyData.js
+++ data/ophthalmologyData.js
@@ -2,23 +2,23 @@ const userData = [
   {
     id: 1,
     photo: "../cdn/ophthalmology/ophthalmology.png",
-    name: "ফিরোজ চক্ষু চিকিৎসা এন্ড আরজুদা অপটিক্স",
-    number: "01934492731",
-    wordNo: "৪",
-    location: "দেলপাড়া উচ্চ বিদ্যালয় এর বিপরীত পাশে।",
-    detail: "চোখের সকল চিকিৎসা করা হয়। ডাক্তার বসেঃ বিকাল ৪ টা থেকে  রাত ৮ টা পর্যন্ত",
-    postLink: "http://fb.com/areaPhonebook",
-    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-  },
-  {
-    id: 2,
-    photo: "../cdn/ophthalmology/ophthalmology.png",
     name: "নয়ন অপটিক্স এবং ওয়াচ",
     number: "01407369470",
     wordNo: "৬",
     location: "হাজী নুরুল ইসলাম সুপার মার্কেট, পাগলা",
     detail: "01948373695 <br>একটি অভিজাত সানগ্লাস, পাওয়া গ্লাস এবং হাত ঘড়ি বিক্রয় কেন্দ্র।",
     postLink: "https://www.facebook.com/Nayon-optics-gallery-105818724656126",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/ophthalmology/ophthalmology.png",
+    name: "ফিরোজ চক্ষু চিকিৎসা এন্ড আরজুদা অপটিক্স",
+    number: "01934492731",
+    wordNo: "৪",
+    location: "দেলপাড়া উচ্চ বিদ্যালয় এর বিপরীত পাশে।",
+    detail: "চোখের সকল চিকিৎসা করা হয়। ডাক্তার বসেঃ বিকাল ৪ টা থেকে  রাত ৮ টা পর্যন্ত",
+    postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
   {
