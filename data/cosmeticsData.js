fd5b4b556bc87ccb483ea829f3b22d5dc33c038e
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-02-28 16:37:16 +0600

    data add online-seller file

---
+++ data/cosmeticsData.js
@@ -0,0 +1,14 @@
+const userData = [
+    {
+      id: 1,
+      photo: "../cdn/cosmetics/cosmetics.png",
+      name: "কুয়াশা",
+      number: "01858584846",
+      wordNo: "৬",
+      location: "পাগলা নয়ামাটি কেন্দ্রীয় জামে মসজিদ সংলগ্ন",
+      detail: "Authentic Helth & Beauty Products.",
+      postLink: "https://www.facebook.com/Kuasha03",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
