68b1d5634b91e365a11a358f2e91b2118b08d4b5
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-04 09:54:32 +0600

    data add in departmentData file

---
+++ data/departmentData.js
@@ -0,0 +1,153 @@
+const cardData = [
+    {
+        id: 1,
+        name: "অফিস অফ দি কন্ট্রোলার জেনারেল অফ একাউন্টস",
+        link: "http://www.cga.gov.bd/",
+    },
+    {
+        id: 2,
+        name: "অফিস অব দি রেজিস্ট্রার অব জয়েন্ট স্টক কোম্পানিজ এন্ড ফার্মস",
+        link: "http://www.roc.gov.bd/",
+    },
+    {
+        id: 3,
+        name: "অ্যাটর্নি জেনারেল অফিস",
+        link: "http://attorneygeneral.gov.bd/",
+    },
+    {
+        id: 4,
+        name: "আইন কমিশন-বাংলাদেশ",
+        link: "http://www.lawcommissionbangladesh.org/",
+    },
+    {
+        id: 5,
+        name: "আন্তর্জাতিক উদরাময় রোগ গবেষণা কেন্দ্র, বাংলাদেশ",
+        link: "https://www.icddrb.org/",
+    },
+    {
+        id: 6,
+        name: "আন্তর্জাতিক মাতৃভাষা ইনস্টিটিউট",
+        link: "http://imli.gov.bd/",
+    },
+    {
+        id: 7,
+        name: "আমদানি ও রপ্তানি নিয়ন্ত্রণ অধিদপ্তর",
+        link: "http://www.ccie.gov.bd/index.php?cmd=about_ccie&id=13",
+    },
+    {
+        id: 8,
+        name: "আমেরিকান ইনস্টিটিউট অব বাংলাদেশ স্টাডিজ",
+        link: "https://aibs.net/",
+    },
+    {
+        id: 9,
+        name: "আর্কাইভস ও গ্রন্থাগার অধিদপ্তর",
+        link: "http://www.nanl.gov.bd/",
+    },
+    {
+        id: 10,
+        name: "ইনভেস্টমেন্ট কর্পোরেশন অব বাংলাদেশ",
+        link: "http://www.icb.gov.bd/zindex.php",
+    },
+    {
+        id: 11,
+        name: "ইনস্টিটিউট অব ওয়াটার মডেলিং",
+        link: "https://www.iwmbd.org/",
+    },
+    {
+        id: 12,
+        name: "ইনস্টিটিউট অব কস্ট এন্ড ম্যানেজমেন্ট একাউন্ট্যান্টস",
+        link: "https://www.icmab.org.bd/",
+    },
+    {
+        id: 13,
+        name: "ইনস্টিটিউট অব চার্টার্ড একাউন্ট্যান্টস অব বাংলাদেশ",
+        link: "https://www.icab.org.bd/",
+    },
+    {
+        id: 14,
+        name: "ইন্টিগ্রেটেড এগ্রিকালচারাল প্রোডাক্টিভিটি প্রজেক্ট",
+        link: "http://www.iapp.gov.bd/",
+    },
+    {
+        id: 15,
+        name: "ইলেক্ট্রিসিটি জেনারেশন কোম্পানি লি.",
+        link: "http://www.egcb.com.bd/",
+    },
+    {
+        id: 16,
+        name: "উচ্চশিক্ষা মানোন্নয়ন প্রকল্প",
+        link: "http://heqep-ugc.gov.bd/",
+    },
+    {
+        id: 17,
+        name: "উপানুষ্ঠানিক শিক্ষা ব্যুরো",
+        link: "http://www.bnfe.gov.bd/",
+    },
+    {
+        id: 18,
+        name: "এনজিও বিষয়ক ব্যুরো",
+        link: "http://www.ngoab.gov.bd/",
+    },
+    {
+        id: 19,
+        name: "ওয়াকফ প্রশাসক",
+        link: "http://www.waqf.gov.bd/",
+    },
+    {
+        id: 20,
+        name: "ওয়েজ আর্নার্স কল্যাণ বোর্ড",
+        link: "http://www.wewb.gov.bd/",
+    },
+    {
+        id: 21,
+        name: "ঔষধ প্রশাসন অধিদপ্তর",
+        link: "www.dgda.gov.bd",
+    },
+    {
+        id: 22,
+        name: "কন্ট্রোলার অব সার্টিফাইং অথরিটিজ",
+        link: "http://www.cca.gov.bd/",
+    },
+    {
+        id: 23,
+        name: "কন্ট্রোলার জেনারেল ডিফেন্স ফাইন্যান্স",
+        link: "www.cgdf.gov.bd",
+    },
+    {
+        id: 24,
+        name: "কারখানা ও স্থাপনা পরিদর্শক",
+        link: "dife.gov.bd/index.php/en/",
+    },
+    {
+        id: 25,
+        name: "কারা অধিদপ্তর",
+        link: "http://www.prison.gov.bd/",
+    },
+    {
+        id: 26,
+        name: "কারিগরী শিক্ষা অধিদপ্তর",
+        link: "www.techedu.gov.bd",
+    },
+    {
+        id: 27,
+        name: "কৃষি তথ্য সার্ভিস (এআইএস)",
+        link: "www.ais.gov.bd",
+    },
+    {
+        id: 28,
+        name: "কৃষি বিপণন অধিদপ্তর",
+        link: "http://www.dam.gov.bd/",
+    },
+    {
+        id: 29,
+        name: "কৃষি সম্প্রসারণ অধিদপ্তর",
+        link: "http://www.dae.gov.bd/",
+    },
+    {
+        id: 30,
+        name: "কেন্দ্রীয় ক্রয় কারিগরি ইউনিট",
+        link: "www.cptu.gov.bd",
+    },
+  ];
+  
