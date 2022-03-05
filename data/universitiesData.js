cb03b8a304d49af8960ceebaa7a99d56630c2547
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-05 20:17:00 +0600

    data corretion

--- data/universitiesData.js
+++ data/universitiesData.js
@@ -9,11 +9,11 @@ const categoryData = [
         [        
           {
             name: "ঢাকা বিশ্ববিদ্যালয়",
-            link: "https://www.du.ac.bd"
+            link: "https://www.du.ac.bd/"
           },
           {
             name: "জগন্নাথ বিশ্ববিদ্যালয়",
-            link: "https://www.jnu.ac.bd"
+            link: "https://www.jnu.ac.bd/"
           },
           {
             name: "জাহাঙ্গীরনগর বিশ্ববিদ্যালয়",
@@ -21,151 +21,151 @@ const categoryData = [
           },
           {
             name: "বাংলাদেশ কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.bau.edu.bd"
+            link: "https://www.bau.edu.bd/"
           },
           {
             name: "বাংলাদেশ প্রকৌশল ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.buet.ac.bd"
+            link: "https://www.buet.ac.bd/"
           },
           {
             name: "চট্টগ্রাম বিশ্ববিদ্যালয়",
-            link: "https://www.cu.ac.bd"
+            link: "https://www.cu.ac.bd/"
           },          
           {
             name: "রাজশাহী বিশ্ববিদ্যালয়",
-            link: "https://www.ru.ac.bd"
+            link: "https://www.ru.ac.bd/"
           },
           {
             name: "ইসলামী বিশ্ববিদ্যালয়, বাংলাদেশ",
-            link: "https://www.iu.ac.bd"
+            link: "https://www.iu.ac.bd/"
           },
           {
             name: "শাহজালাল বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.sust.edu"
+            link: "https://www.sust.edu/"
           },
           {
             name: "খুলনা বিশ্ববিদ্যালয়",
-            link: "https://www.ku.ac.bd"
+            link: "https://www.ku.ac.bd/"
           },
           {
             name: "বাংলাদেশ উন্মুক্ত বিশ্ববিদ্যালয়",
-            link: "https://www.bou.ac.bd"
+            link: "https://www.bou.ac.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিব মেডিকেল বিশ্ববিদ্যালয়",
-            link: "https://www.bsmmu.edu.bd"
+            link: "https://www.bsmmu.edu.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.bsmrau.edu.bd"
+            link: "https://www.bsmrau.edu.bd/"
           },
           {
             name: "হাজী মোহাম্মদ দানেশ বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.hstu.ac.bd"
+            link: "https://www.hstu.ac.bd/"
           },
           {
             name: "মাওলানা ভাসানী বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.mbstu.ac.bd"
+            link: "https://www.mbstu.ac.bd/"
           },
           {
             name: "পটুয়াখালী বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.pstu.ac.bd"
+            link: "https://www.pstu.ac.bd/"
           },
           {
             name: "শেরেবাংলা কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.sau.edu.bd"
+            link: "https://www.sau.edu.bd/"
           },
           {
             name: "চট্টগ্রাম প্রকৌশল ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.cuet.ac.bd"
+            link: "https://www.cuet.ac.bd/"
           },
           {
             name: "রাজশাহী প্রকৌশল ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.ruet.ac.bd"
+            link: "https://www.ruet.ac.bd/"
           },
           {
             name: "খুলনা প্রকৌশল ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.kuet.ac.bd"
+            link: "https://www.kuet.ac.bd/"
           },
           {
             name: "ঢাকা প্রকৌশল ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.duet.ac.bd"
+            link: "https://www.duet.ac.bd/"
           },
           {
             name: "নোয়াখালী বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.nstu.edu.bd"
+            link: "https://www.nstu.edu.bd/"
           },
           {
             name: "কুমিল্লা বিশ্ববিদ্যালয়",
-            link: "https://www.cou.ac.bd"
+            link: "https://www.cou.ac.bd/"
           },
           {
             name: "জাতীয় কবি কাজী নজরুল ইসলাম বিশ্ববিদ্যালয়",
-            link: "https://www.jkkniu.edu.bd"
+            link: "https://www.jkkniu.edu.bd/"
           },
           {
             name: "চট্টগ্রাম ভেটেরিনারি ও এনিম্যাল সায়েন্সেস বিশ্ববিদ্যালয়",
-            link: "https://www.cvasu.ac.bd"
+            link: "https://www.cvasu.ac.bd/"
           },
           {
             name: "সিলেট কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.sau.ac.bd"
+            link: "https://www.sau.ac.bd/"
           },
           {
             name: "যশোর বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.just.edu.bd"
+            link: "https://www.just.edu.bd/"
           },
           {
             name: "পাবনা বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.pust.ac.bd"
+            link: "https://www.pust.ac.bd/"
           },
           {
             name: "বেগম রোকেয়া বিশ্ববিদ্যালয়, রংপুর",
-            link: "https://www.brur.ac.bd"
+            link: "https://www.brur.ac.bd/"
           },
           {
             name: "বাংলাদেশ ইউনিভার্সিটি অফ প্রফেশনালস",
-            link: "https://www.bup.edu.bd"
+            link: "https://www.bup.edu.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.bsmrstu.edu.bd"
+            link: "https://www.bsmrstu.edu.bd/"
           },
           {
             name: "বাংলাদেশ ইউনিভার্সিটি অব টেক্সটাইল",
-            link: "https://www.butex.edu.bd"
+            link: "https://www.butex.edu.bd/"
           },
           {
             name: "বরিশাল বিশ্ববিদ্যালয়",
-            link: "https://www.bu.ac.bd"
+            link: "https://www.bu.ac.bd/"
           },
           {
             name: "রাঙামাটি বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
-            link: "https://www.rmstu.edu.bd"
+            link: "https://www.rmstu.edu.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান মেরিটাইম ইউনিভার্সিটি, বাংলাদেশ",
-            link: "https://www.bsmrmu.edu.bd"
+            link: "https://www.bsmrmu.edu.bd/"
           },
           {
             name: "ইসলামী আরবি বিশ্ববিদ্যালয়",
-            link: "https://www.iau.edu.bd"
+            link: "https://www.iau.edu.bd/"
           },
           {
             name: "চট্টগ্রাম মেডিকেল বিশ্ববিদ্যালয়",
-            link: "https://www.cmu.edu.bd"
+            link: "https://www.cmu.edu.bd/"
           },
           {
             name: "রাজশাহী মেডিকেল বিশ্ববিদ্যালয়",
-            link: "https://www.rmu.edu.bd"
+            link: "https://www.rmu.edu.bd/"
           },
           {
             name: "রবীন্দ্র বিশ্ববিদ্যালয়, বাংলাদেশ",
-            link: "https://www.rub.ac.bd"
+            link: "https://www.rub.ac.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান ডিজিটাল বিশ্ববিদ্যালয়, বাংলাদেশ",
-            link: "https://www.bdu.ac.bd"
+            link: "https://www.bdu.ac.bd/"
           },
           {
             name: "শেখ হাসিনা বিশ্ববিদ্যালয়",
@@ -173,7 +173,7 @@ const categoryData = [
           },
           {
             name: "খুলনা কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.kau.edu.bd"
+            link: "https://www.kau.edu.bd/"
           },
           {
             name: "বঙ্গমাতা শেখ ফজিলাতুন্নেছা মুজিব বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
@@ -185,7 +185,7 @@ const categoryData = [
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান এভিয়েশন অ্যান্ড অ্যারোস্পেস ইউনিভার্সিটি (BSMRAU)",
-            link: "https://www.bsmraau.edu.bd"
+            link: "https://www.bsmraau.edu.bd/"
           },
           {
             name: "বঙ্গবন্ধু শেখ মুজিবুর রহমান বিশ্ববিদ্যালয়, কিশোরগঞ্জ",
@@ -193,7 +193,7 @@ const categoryData = [
           },
           {
             name: "হবিগঞ্জ কৃষি বিশ্ববিদ্যালয়",
-            link: "https://www.hau.ac.bd"
+            link: "https://www.hau.ac.bd/"
           },
           {
             name: "চাঁদপুর বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়",
