9ec550298b07edee27859a6aceb5331501296974
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-05 11:15:23 +0600

    data add and remove

---
+++ data/sports-organizationsData.js
@@ -0,0 +1,80 @@
+const userData = [
+    {
+        id: 1,
+        photo: "../cdn/sports-organizations/tushar-khelar-ghor.png",
+        name: "তুষার খেলাঘর আসর",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 2,
+        photo: "../cdn/sports-organizations/kutubpur-lal-sobuj-sporting-club.png",
+        name: "কুতুবপুর লাল সবুজ স্পোর্টিং ক্লাব",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 3,
+        photo: "../cdn/sports-organizations/daulatpur-sports-association.png",
+        name: "দৌলতপুর ক্রীড়া সংস্থা",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 4,
+        photo: "../cdn/sports-organizations/anirban-sports-organization.png",
+        name: "অনির্বান ক্রীড়া সংগঠন",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 5,
+        photo: "../cdn/sports-organizations/kutubpur-sporting-club.png",
+        name: "কুতুবপুর স্পোর্টিং ক্লাব",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 6,
+        photo: "../cdn/sports-organizations/matrichaya-sports-circle.png",
+        name: "মাতৃছায়া ক্রীড়া চক্র",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    },
+    {
+        id: 7,
+        photo: "../cdn/sports-organizations/khelaghor.png",
+        name: "খেলাঘর",
+        number: "01798899911",
+        wordNo: "সমগ্র কুতুবপুর",
+        location: "সমগ্র কুতুবপুর",
+        detail: "ক্রীড়া সংগঠন।",
+        postLink: "http://fb.com/areaPhonebook",
+        videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
+    }
+];
+  
