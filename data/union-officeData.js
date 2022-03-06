c92736d17d724bb3bce5dcc0354a3ef4058a468a
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-06 11:00:06 +0600

    data add in union-officeData file

--- data/union-officeData.js
+++ data/union-officeData.js
@@ -9,5 +9,16 @@ const userData = [
     detail: "kutubpurup@gmail.com <br>কুতুবপুর ইউনিয়ন এর সচিব।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/union-office/bangladesh-postal-department.png",
+    name: "মোঃ এনামুল হক দিপু",
+    number: "01929066782",
+    wordNo: "সার্কেল ইউনিট, নারায়ণগঞ্জ",
+    location: "কুতুবপুর ইউনিয়ন",
+    detail: "মোঃ এনামুল হক দিপু (সারাধন সম্পাদক) <br> বাংলাদেশ চতুর্থশ্রেনী সরকারী কর্মচারী সমিতি 'বাংলাদেশ ডাক বিভাগ'।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
