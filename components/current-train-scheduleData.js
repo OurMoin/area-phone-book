4e80f4b29116fcac15a089b5a4944479b905501b
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-08 12:27:35 +0600

    link remove

--- components/current-train-scheduleData.js
+++ components/current-train-scheduleData.js
@@ -9,13 +9,13 @@ const categoryData = [
       id: 2,
       photo: "../resources/icons/current-train-schedule/eastern-train.png",
       name: "পূর্বাঞ্চল",
-      link: "https://railway.portal.gov.bd/site/page/988258c9-5f11-4719-91e2-fbc898d4c2a9"
+      link: "#"
     },
     {
       id: 3,
       photo: "../resources/icons/current-train-schedule/western-train.png",
       name: "পশ্চিমাঞ্চল",
-      link: "https://railway.portal.gov.bd/site/page/6f618d21-8c9a-41da-a896-d5ed83b53b17"
+      link: "#"
     }
   ];
   
