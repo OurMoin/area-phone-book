5d41477d6152ad1b5ff45287d3b8831e1169b790
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-12 12:22:17 +0600

    id change

--- data/social-organizationData.js
+++ data/social-organizationData.js
@@ -198,7 +198,7 @@ const userData = [
         videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&app"
     },
     {
-        id: 52,
+        id: 15,
         photo: "../cdn/social-organization/sobuj-kuri-bangladesh.png",
         name: "সবুজ কুঁড়ি বাংলাদেশ",
         number: "01620740528",
