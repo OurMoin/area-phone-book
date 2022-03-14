b9115b5bec474a3a6b3dff82efd71e48bc621127
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-14 17:44:53 +0600

    data add in computer-shopData

--- data/computer-shopData.js
+++ data/computer-shopData.js
@@ -9,6 +9,17 @@ const userData = [
       detail: "Proprietor : Md. Sajjat Hossain Shawon<br> কম্পিউটার কম্পোজ, ফটোকপি, অনলাইন আবেদন, ছবি থেকে ছবি সহ যাবতীয় কাজ করা হয়।",
       postLink: "http://fb.com/areaPhonebook",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/computer-shop/sr-computer-center.jpg",
+      name: "আব্দুল কাদির কম্পিউটার",
+      number: "01785808040",
+      wordNo: "অন্যান্য",
+      location: "ফতুল্লা থানা সংলগ্ন, আমির সুপার মার্কেট",
+      detail: "থানার বিভিন্ন প্রকার আবেদনের কম্পোজ, জন্ম নিবন্ধন, ভোটার কার্ড, ড্রাইভিং লাইসেন্স, পাসপোর্ট এর আবেদন ও সংশোধন এর কাজ করা হয়।",
+      postLink: "http://fb.com/areaPhonebook",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
