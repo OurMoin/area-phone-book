a7481f1dd59a93fcd303078d229ea25d69f1acaf
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-14 17:49:20 +0600

    data add in foodData file

--- data/foodData.js
+++ data/foodData.js
@@ -20,6 +20,17 @@ const userData = [
     detail: "অর্গানিক ফুড, খাঁটি গাওয়া ঘি, খেজুরের গুড়, আখের গুড়, হাতে তৈরি আখের লাল চিনি, বিভিন্ন ফুলের মধু, প্রাকৃতিক চাকের মধু, লাল আটা, লাল চাল, কাঠের ঘানি ভাঙা সরিষার তেল, কালোজিরার তেল, বগুড়ার দই, ক্ষিরসা, ড্রাই ফ্রুটস, মধুময় বাদাম সহ আরো বেশকিছু অর্গানিক খাবার পাবেন আমার কাছে।",
     postLink: "https://www.facebook.com/ashraf2240",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+   },
+   {
+    id: 3,
+    photo: "../cdn/food/organic-food-point.jpg",
+    name: "Furqan - ফুরক্বান",
+    number: "01787178833",
+    wordNo: "৫",
+    location: "পাগলা",
+    detail: "অর্গানিক ফুড (ঘি, মাখন, নারিকেল তেল, অলিভ অয়েল, কালোজিরা তেল, বিভিন্ন ফুলের মধু, ড্রাই ফ্রুটস, খেজুর ইত্যাদি অর্গানিক ফুড।) পাওয়া যায়।",
+    postLink: "https://www.facebook.com/WeAreFurqan",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
    }
 ];
     
