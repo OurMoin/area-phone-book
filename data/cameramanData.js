089fcaaaec34b77c5bcd267cdd150f6e210e1fba
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-15 10:58:41 +0600

    data add in

--- data/cameramanData.js
+++ data/cameramanData.js
@@ -20,5 +20,16 @@ const userData = [
     detail: "01913664120  <br>৬ বছর এর অভিজ্ঞতা প্রাপ্ত। <br>প্রফেশনাল ভিডিও ক্যামেরা মেন ও ফটোগ্রাফার।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/cameraman/cameraman.png",
+    name: "Jabin Photography",
+    number: "01624214416",
+    wordNo: "৬",
+    location: "নয়ামাটি",
+    detail: "সকল ধরনের প্রোগ্রামের ছবি তোলা হয়।",
+    postLink: "https://www.facebook.com/jabinphotography/",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
