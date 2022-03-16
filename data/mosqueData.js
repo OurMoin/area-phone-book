eef26bdbff96843fd63fc4d61733bba3dc1b21d8
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-16 12:44:37 +0600

    data add in mosque

--- data/mosqueData.js
+++ data/mosqueData.js
@@ -1,5 +1,16 @@
 const userData = [
     {
+      id: 24,
+      photo: "../cdn/mosque/mosque.png",
+      name: "সরদার বাড়ি জমে মাসজিদে (দেলপাড়া)",
+      number: "01798899911",
+      wordNo: "৫",
+      location: "শাহী বাজার আমতলা",
+      detail: "ধার্মিক বিষয় সকল তথ্য দেয়া হয় ইনশাআল্লাহ নামাজ সহ সকল নূরানী সহীহ সুদ্ধ ভাবে কোরআন পড়ানো হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
       id: 1,
       photo: "../cdn/mosque/shahi-madina-jame-mosque.jpg",
       name: "শাহী মদীনা জামে মসজিদ",
