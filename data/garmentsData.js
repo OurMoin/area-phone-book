7198b32571fc297ff79b4a86ed6af647fa953e1a
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-19 20:48:01 +0600

    data add in garments

--- data/garmentsData.js
+++ data/garmentsData.js
@@ -20,6 +20,17 @@ const userData = [
       detail: "গার্মেন্টস এর সকল কাজ করা হয়।",
       postLink: "https://www.facebook.com/AreaPhonebook/",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 3,
+      photo: "../cdn/garments/garments.png",
+      name: "নিঝর গার্মেন্টস",
+      number: "01924157444",
+      wordNo: "৬",
+      location: "পাগলা পশ্চিম পাড়া",
+      detail: "গার্মেন্টস এর সকল কাজ করা হয়।",
+      postLink: "https://www.facebook.com/AreaPhonebook/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
