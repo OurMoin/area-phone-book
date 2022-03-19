73cd2579029fdeefb97c5177da7dcf9fe5a45ba1
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-19 19:31:16 +0600

    data add in cosmeticsData file

--- data/cosmeticsData.js
+++ data/cosmeticsData.js
@@ -9,6 +9,17 @@ const userData = [
       detail: "Authentic Helth & Beauty Products.",
       postLink: "https://www.facebook.com/Kuasha03",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/cosmetics/soha's-mamma-dream-house.jpg",
+      name: "Soha's Mamma Dream House",
+      number: "01833988353",
+      wordNo: "৫",
+      location: "শাহী বাজার",
+      detail: "হলুদের গহনা, ব্রাইডাল, নন ব্রাইডাল সেট, খোপা, কালিরা হিজাব পিন ও গাজরা পাওয়া যায়। এখানে গায়ে হলুদের গহনা, বিয়ের গোল্ড প্লেটেড গহনা, শাড়ি, ব্রাইডাল চুলের খোপা সবকিছু পাবেন।",
+      postLink: "https://www.facebook.com/Sohasdreamhouse/",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
