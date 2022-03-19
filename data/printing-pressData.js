4e6aa9c1a9b0ce2aa9137c05a67b1fa00f93ee73
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-19 18:29:48 +0600

    data add in printing-pressData file

--- data/printing-pressData.js
+++ data/printing-pressData.js
@@ -9,5 +9,27 @@ const userData = [
     detail: "কম খরচে ব্যানার প্রিন্ট করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 2,
+    photo: "../cdn/printing-press/printing-press.png",
+    name: "A. ALI PRINTING PRESS",
+    number: "01924716856",
+    wordNo: "৬",
+    location: "বাইতুল ফালাহ জামে মসজিদ মার্কেট, পাগলা",
+    detail: "কার্ড, ব্যানার, পোস্টার, প্রিন্টিং এর সকল কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/printing-press/printing-press.png",
+    name: "মা গ্রাফিক্স এন্ড সোহাগ এড",
+    number: "01818218137",
+    wordNo: "৬",
+    location: "পাগলা মসজিদ মার্কেট",
+    detail: "01710727500 <br> sohagmgp85@gmail.com<br> কার্ড, ব্যানার, পোস্টার ও প্রিন্টিং সংক্রান্ত সকল কাজ করা হয়।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
