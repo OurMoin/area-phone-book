a04dcc8384ea88c0ae64e1da85d5c09928d6a35c
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-30 19:11:17 +0600

    data add in blood-donorData file

--- data/blood-donorData.js
+++ data/blood-donorData.js
@@ -28,7 +28,7 @@ const userData = [
     number: "01956122776",
     wordNo: "৬",
     location: "নয়ামাটি, পাগলা",
-    detail: "রক্ত দান",
+    detail: "রক্তদাতা।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   },
@@ -63,6 +63,160 @@ const userData = [
     location: "ভুইগড়",
     detail: "রক্তের গ্রুপ O+ <br>প্রতি ৪ মাস পর পর রক্তদান করা হয়।",
     postLink: "https://www.facebook.com/sishawon.hossan",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 7,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মোঃ ইয়াসিন আরাফাত সেমন",
+    number: "01819319086",
+    wordNo: "২",
+    location: "ভুইগড়",
+    detail: "রক্তদাতা।",
+    postLink: "arafatsemon1@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মোঃআব্দুল হক",
+    number: "01990232823",
+    wordNo: "৬",
+    location: "বৌবাজার, বটতলা",
+    detail: "মুমুর্ষ রোগীর জন্য রক্ত জোগার করে দিয়ে থাকি।",
+    postLink: "https://www.facebook.com/profile.php?id=100010753004254",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 9,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "রোকন উদ্দিন রনি",
+    number: "01681475340",
+    wordNo: "৬",
+    location: "দক্ষিণ নয়ামাটি",
+    detail: "সেচ্ছায় রক্ত দান করে থাকি। রক্তের গ্রুপঃ এ পজিটিভ।",
+    postLink: "https://www.facebook.com/rokonuddinr",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 10,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মোঃ শান্ত",
+    number: "01626415362",
+    wordNo: "৫",
+    location: "রসুলপুর",
+    detail: "রক্তদাতা",
+    postLink: "shantohossain614@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 11,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "ইয়াসিন আহমেদ",
+    number: "01868385151",
+    wordNo: "৬",
+    location: "নয়ামাটি",
+    detail: "রক্তদাতা",
+    postLink: "https://www.facebook.com/yeasin.ahmed.9461799",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 12,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মাতৃছায়া ব্লাড গ্রুপ",
+    number: "01719796524",
+    wordNo: "৫",
+    location: "শাহী মহল্লা ",
+    detail: "রক্তদাতা",
+    postLink: "Jasim.rrmgroup@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 13,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "কাওসার সিকদার মোহন",
+    number: "01935180157",
+    wordNo: "২",
+    location: "ভুইগড়",
+    detail: "রক্তদাতা",
+    postLink: "kawsersikder3214@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 14,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "হাবিবুর রহমান অনিক",
+    number: "01930086518",
+    wordNo: "৫",
+    location: "শাহী বাজার",
+    detail: "রক্তদাতা",
+    postLink: "https://www.facebook.com/profile.php?id=100008453406465",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 15,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "ব্লাড স্কোয়াড",
+    number: "01749990178",
+    wordNo: "২",
+    location: "ভূইগড়",
+    detail: "স্বেচ্ছায় রক্তদান ও সমাজসেবী সংগঠন।",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 16,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মেহেদী হাসান হৃদয়",
+    number: "01842996779",
+    wordNo: "৬",
+    location: "রসুলপুর",
+    detail: "রক্তদাতা",
+    postLink: "lxhridoy2019@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 17,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "নাজমুল অনিক",
+    number: "01905547921",
+    wordNo: "৮",
+    location: "নন্দলালপুর",
+    detail: "রক্তদাতা",
+    postLink: "hawlader.najmul.anik@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 18,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মো:সিফাত আহমেদ",
+    number: "01406990357",
+    wordNo: "৩",
+    location: "ভূইগড় পশ্চিমপাড়া",
+    detail: "রক্তদাতা",
+    postLink: "https://www.facebook.com/mdsifat.ahmed.144",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 19,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "আল-আমিন",
+    number: "01937185496",
+    wordNo: "৬",
+    location: "পাগলা, রেললাইন",
+    detail: "রক্তদাতা",
+    postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 20,
+    photo: "../cdn/blood-donor/man-blood-donor.png",
+    name: "মোঃ শাহরিয়ার লিংকন",
+    number: "01811738885",
+    wordNo: "৪",
+    location: "পাগলা, রেললাইন",
+    detail: "রক্তের গ্রুপঃ বি পজিটিভ।",
+    postLink: "shahriar0156@gmail.com",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
