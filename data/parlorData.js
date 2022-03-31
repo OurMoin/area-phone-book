d6b9dd95d4b8fccd9f5e0fca65ba8200e9ce0380
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-31 11:18:22 +0600

    data add

--- data/parlorData.js
+++ data/parlorData.js
@@ -72,8 +72,19 @@ const userData = [
     number: "01681767839",
     wordNo: "৬",
     location: "পশ্চিম রসুলপুর, ভাংগাপুল মসজিদ সংলগ্ন",
-    detail: "syasmin7839@gmail.com <br>অতি যত্ন সহকারে যে কোনো ধরনের সৌন্দর্য চর্চার কাজ করা হয়।<br> ফেসিয়াল, স্পা, রিবন্ডিং, পার্টি সাজ, বৌ সাজ, মেনিকিওর, পেডিকিওর সহ যাবতীয় রুপচর্চার কাজ।",
+    detail: "syasmin7839@gmail.com <br>অতি যত্ন সহকারে যে কোনো ধরনের সৌন্দর্য চর্চার কাজ করা হয়।<br> ফেসিয়াল, স্পা, রিবন্ডিং, পার্টি সাজ, বৌ সাজ, মেনিকিওর, পেডিকিওর সহ যাবতীয় রুপচর্চার কাজ করা হয়।",
     postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 8,
+    photo: "../cdn/parlor/any's-parlour-&-training-center.jpg",
+    name: "Any's parlour and training center",
+    number: "01960199806",
+    wordNo: "৬",
+    location: "নুরবাগ শাহী মহল্লা",
+    detail: "সব ধরনের মেকআপ, চুল কাটিং, চুল বাঁধা, মেহেদী, ফেসিয়াল, এবং অন্যান্য সব ধরনের কাজ করা হয়।",
+    postLink: "https://www.facebook.com/Anysparlourcenter/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
