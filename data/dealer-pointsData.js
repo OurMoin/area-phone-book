da30ddc9df9bb649eee9d20ea283970f688917b6
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-31 13:44:03 +0600

    data add in dealer-points

--- data/dealer-pointsData.js
+++ data/dealer-pointsData.js
@@ -1,7 +1,7 @@
 const userData = [
   {
     id: 1,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/dealer-points/dealer-points.png",
     name: "বসুন্ধরা ও মেঘনা গ্রুপ",
     number: "01979126418",
     wordNo: "৬",
@@ -12,7 +12,7 @@ const userData = [
   },
   {
     id: 2,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/dealer-points/dealer-points.png",
     name: "ACI, মমতাজ",
     number: "01961695060",
     wordNo: "৬",
@@ -23,7 +23,7 @@ const userData = [
   },
   {
     id: 3,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/dealer-points/dealer-points.png",
     name: "SMC,জয়া",
     number: "01961695060",
     wordNo: "৬",
@@ -34,7 +34,7 @@ const userData = [
   },
   {
     id: 4,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/dealer-points/dealer-points.png",
     name: "কুমারিকা, কোকোলা",
     number: "01961695060",
     wordNo: "৬",
@@ -45,13 +45,24 @@ const userData = [
   },
   {
     id: 5,
-    photo: "../cdn/wholesale-store/wholesale-store.png",
+    photo: "../cdn/dealer-points/dealer-points.png",
     name: "উনিভারসেল, ট্রিট ব্লেড",
     number: "01961695060",
     wordNo: "৬",
     location: "এস এম মার্কেট, পাগলা",
     detail: "ইউনিভারসাল - স্যালাইন, ট্রেইট ব্লেট - ব্লেট, কুমারিকা তেল, কোকোলা নুডুলস আড়ং দুধ এর সকল প্রোডাক্ট এর ডিলার পয়েন্ট।",
     postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 6,
+    photo: "../cdn/dealer-points/niyamat-enterprise.jpg",
+    name: "নিয়ামত এন্টারপ্রাইজ",
+    number: "01832451262",
+    wordNo: "৬",
+    location: "পশ্চিম নয়ামাটি",
+    detail: "RO, UV, UC Water Filter বিক্রি এবং মেরামত। সকল প্রকার পানির প্লান্ট বিক্রি ও মেরামত। পানির বিশুদ্ধতা নিশ্চিতকরনে যাবতীয় পানির ফিল্টার, কমার্শিয়াল সকল প্রকার পানির প্লান্ট ও সকল প্রকার যন্ত্রাংশ, ও মেডিসিন পাইকারি ও খুচরা বিক্রি করা হয়।",
+    postLink: "https://www.facebook.com/Niyamat.Online.Shopping/",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
