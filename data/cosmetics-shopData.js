69ffa6df2a56bafaa81a68d576108d400b29e93f
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-31 11:03:12 +0600

    cosmetics-shop file data add

--- data/cosmetics-shopData.js
+++ data/cosmetics-shopData.js
@@ -20,5 +20,27 @@ const userData = [
     detail: "বিউটি পার্লার এন্ড কসমেটিক্স এর দোকান।",
     postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/cosmetics-shop/cosmetics-shop.png",
+    name: "ভাই ভাই কসমেটিক্স",
+    number: "01795812955",
+    wordNo: "৬",
+    location: "হাজী নুরুল ইসলাম সুপার মার্কেট",
+    detail: "কসমেটিক্স আইটেম এর দোকান।",
+    postLink: "https://www.facebook.com/profile.php?id=100011361165411",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/cosmetics-shop/cosmetics-shop.png",
+    name: "আল্লার দান কসমেটিক এন্ড চকলেট হোম",
+    number: "01822764978",
+    wordNo: "৫",
+    location: "নয়ামাটির ঢাল মুসলিম পাড়া",
+    detail: "কসমেটিক, চকলেট ইত্যাদি পন্য পাওয়া যায়।",
+    postLink: "https://www.facebook.com/profile.php?id=100011361165411",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
