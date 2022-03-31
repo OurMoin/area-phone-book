5e5c4aba1f62b4f0289f3af364e23865792fd46e
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-03-31 14:02:01 +0600

    cemetery data add

--- data/cemeteryData.js
+++ data/cemeteryData.js
@@ -31,7 +31,17 @@ const userData = [
       detail: "ভূইগড় কেন্দ্রীয় ঈদগাহ ডি. এন. ডি তথা সমগ্র নারায়ণগঞ্জের সর্ববৃহৎ ঈদগাহ মাঠ। পাশাপাশি সকল শ্রেণীর মুসল্লীদের দাফন করার জন্য বৃহৎ কবরস্থান।",
       postLink: "http://fb.com/areaPhonebook",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
-    }
-    
+    },
+    {
+      id: 4,
+      photo: "../cdn/cemetery/cemetery.png",
+      name: "মো:আকাশ",
+      number: "01749365863",
+      wordNo: "৫",
+      location: "শাহী বাজার কবরস্থান ",
+      detail: "কবরস্থান এর সকল কাজ করা হয়।",
+      postLink: "https://www.facebook.com/profile.php?id=100013829031443",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }    
   ];
   
