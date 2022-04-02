42e0b5836ed815eba05f3bbb3b0a0e70dbfc3425
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-04-02 16:53:54 +0600

    update lowyer

---
+++ data/lawyerData.js
@@ -0,0 +1,14 @@
+const userData = [  
+    {
+      id: 1,
+      photo: "../cdn/lawyer/advSumon.jpg",
+      name: "এড. সুমন মন্ডল (জজ কোর্ট, নারায়ণগঞ্জ)",
+      number: "01842225080",
+      wordNo: "৬",
+      location: "মসজিদ মার্কেট (৩ তলা),  পাগলা বাজার",
+      detail: "Email: sumonmondaladv@gmail.com <br> দেওয়ানী,  ফৌজদারি  ও  পারিবারিক মামলা বিষয়ক আইনজীবী।  ",
+      postLink: "http://fb.com/areaPhonebook",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    }
+  ];
+  
