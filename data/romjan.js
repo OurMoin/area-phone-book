8990219d278bf6ba74293a1be45a2ce9bfdb55e4
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-04-02 15:39:55 +0600

    romadan calander update

---
+++ data/romjan.js
@@ -0,0 +1,26 @@
+data = {
+    "2022-04-02": {
+      "date": "০৩ এপ্রিল ২০২২",
+      "ramadan": "০১",
+      "day": "সোমবার",
+      "sahri": "৪.৩২",
+      "iftar": "৬.১০"
+    },
+    "2022-04-03": {
+        "date": "০৩ এপ্রিল ২০২২",
+        "ramadan": "০১",
+        "day": "সোমবার",
+        "sahri": "৪.৩২",
+        "iftar": "৬.১০"
+    }
+  }
+  
+  today = new Date().toISOString().split('T')[0]
+  todays_data = data[today]
+  
+  $("#ramadan").text(todays_data.ramadan)
+  $("#date").text(todays_data.date)
+  $("#day").text(todays_data.day)
+  $("#sahri").text(todays_data.sahri)
+  $("#iftar").text(todays_data.iftar)
+  
