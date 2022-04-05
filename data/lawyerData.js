677b1d024d817fb6af3ae8ff13b4df9cc3486c65
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-04-05 23:39:51 +0600

    data add lawyer file

--- data/lawyerData.js
+++ data/lawyerData.js
@@ -9,6 +9,39 @@ const userData = [
       detail: "Email: sumonmondaladv@gmail.com <br> দেওয়ানী, ফৌজদারি ও পারিবারিক মামলা বিষয়ক আইনজীবী।",
       postLink: "https://www.facebook.com/sumon.mondal.940",
       videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 2,
+      photo: "../cdn/lawyer/lawyer.png",
+      name: "রবিন হোসেন",
+      number: "01674727017",
+      wordNo: "৬",
+      location: "নয়ামাটি",
+      detail: "Email: Mdrh3344@gmail.com <br> দেওয়ানী, ফৌজদারি এবং ট্যাক্স মামলা বিষয়ক আইনজীবী।",
+      postLink: "Mdrh3344@gmail.com",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 3,
+      photo: "../cdn/lawyer/lawyer.png",
+      name: "Bhuiyan Tax & VAT Solutions",
+      number: "01913224005",
+      wordNo: "৪",
+      location: "কুসুমবাগ, চিতাশাল",
+      detail: "Email: mkamrul003@gmail.com <br>আয়কর, (ব্যক্তি বা কোম্পানি), ই-টিন, ভ্যাট, কোম্পানি গঠন, বিআইডিএ নিবন্ধন, ভিসা, ট্রেড লাইসেন্স, ওয়ার্ক পারমিট এক্সটেনশন, প্রকল্প প্রোফাইল, সম্পদ মূল্যায়ন।",
+      postLink: "mkamrul003@gmail.com",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+    },
+    {
+      id: 4,
+      photo: "../cdn/lawyer/lawyer.png",
+      name: "মোঃ রাশেদুর রহমান (মিন্টু)",
+      number: "01913508023",
+      wordNo: "৫",
+      location: "শাহী বাজার ",
+      detail: "Email: rahmanmintu.m@gmail.com <br>ভূমি সংক্রান্ত যেকোন পরামর্শ দেয়া হয় এবং ভূমি রেজিষ্ট্রেশন করতে সহায়তা করা হয়।",
+      postLink: "rahmanmintu.m@gmail.com",
+      videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
     }
   ];
   
