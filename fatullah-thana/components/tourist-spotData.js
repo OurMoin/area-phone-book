03658e193d7f203446a88b59b8d95e82b76848c9
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-04-11 00:15:14 +0600

    folder add

---
+++ fatullah-thana/components/tourist-spotData.js
@@ -0,0 +1,33 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/educational-institutions/park.png",
+      name: "পার্ক",
+      link: "park.html"
+    },  
+    {
+      id: 2,
+      photo: "../resources/icons/educational-institutions/cinema-hall.png",
+      name: "সিনেমা হল",
+      link: "cinema-hall.html"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/educational-institutions/restaurant.png",
+      name: "রেস্টুরেন্ট",
+      link: "restaurant.html"
+    },
+    {
+      id: 4,
+      photo: "../resources/icons/educational-institutions/stadium.png",
+      name: "স্টোডিয়াম",
+      link: "stadium.html"
+    },
+    {
+      id: 5,
+      photo: "../resources/icons/educational-institutions/historic-place.png",
+      name: "ঐতিহাসিক স্থান",
+      link: "historic-place.html"
+    }
+  ];
+  
---
+++ fatullah-thana/components/train-time-tableData.js
@@ -0,0 +1,21 @@
+const categoryData = [
+    {
+      id: 1,
+      photo: "../resources/icons/train-time-table/fatullah-narayanganj-train.png",
+      name: "ফতুল্লা - নারায়ণগঞ্জ",
+      link: "fatullah-narayanganj-train.html"
+    },  
+    {
+      id: 2,
+      photo: "../resources/icons/train-time-table/fatullah-dhaka-train.png",
+      name: "ফতুল্লা - ঢাকা",
+      link: "fatullah-dhaka-train.html"
+    },
+    {
+      id: 3,
+      photo: "../resources/icons/train-time-table/deputy-commissioner.png",
+      name: "ফতুল্লা থানাধিন স্টেশন",
+      link: "station-under-fatullah-police-station.html"
+    }
+  ];
+  
