508fb4c5dc9cba696a79a99fdce4124a412bd2c8
Moin <ourmoin@gmail.com>
2022-04-11 11:36:45 +0600

    construction-engineering data add

--- data/construction-engineeringData.js
+++ data/construction-engineeringData.js
@@ -20,6 +20,17 @@ const userData = [
     detail: "Engr. Saiful Islam Shamim <br>সয়েল টেস্ট, বিল্ডিং ডিজাইন, রাজউক উপজেলা অনুমোদন, বিল্ডিংয়ের এস্টিমেট কস্টিং, ইমারত নির্মান ঠিকাদারী।",
     postLink: "https://www.facebook.com/saiful.islamshamim.9693",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 3,
+    photo: "../cdn/construction-engineering/saiful-islam-shamim.jpg",
+    name: "নুসরাত  কনস্ট্রাশন",
+    number: "01743839322",
+    wordNo: "৩",
+    location: "ভূইগড় পশ্চিম পাড়া",
+    detail: "hellobossrb@gmail.com <br>Building, hicall, labour contract, finishing work Etc.",
+    postLink: "hellobossrb@gmail.com",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fyiralcrazy%2Fvideos%2F614052535735551%2F&width=500&show_text=false&height=280&appId"
   }
 ];
   
