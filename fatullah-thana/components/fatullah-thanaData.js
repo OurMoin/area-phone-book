ee7e5c2ba806b2a6eff5cc86e928ca32d341e48a
Moin <ourmoin@gmail.com>
2022-04-15 23:25:05 +0600

    cover photo add

--- fatullah-thana/components/fatullah-thanaData.js
+++ fatullah-thana/components/fatullah-thanaData.js
@@ -1,39 +1,37 @@
-const categoryData = [
-    {
-      id: 1,
-      photo: "../resources/icons/fatullah-thana/fatullah-police-station.png",
-      name: "ফতুল্লা পুলিশ স্টেশন",
-      link: "fatullah-police-station.html"
-    },  
-    {
-      id: 2,
-      photo: "../resources/icons/fatullah-thana/naval-police.png",
-      name: "নৌ পুলিশ",
-      link: "naval-police.html"
+const categoryData = [{
+        id: 1,
+        photo: "../resources/icons/fatullah-thana/police-station.png",
+        name: "ফতুল্লা পুলিশ স্টেশন",
+        link: "fatullah-police-station.html"
     },
     {
-      id: 3,
-      photo: "../resources/icons/fatullah-thana/industrial-police.png",
-      name: "ইন্ডাস্টিয়াল পুলিশ",
-      link: "industrial-police.html"
+        id: 2,
+        photo: "../resources/icons/fatullah-thana/naval-police.png",
+        name: "নৌ পুলিশ",
+        link: "naval-police.html"
     },
     {
-      id: 4,
-      photo: "../resources/icons/fatullah-thana/pbi.png",
-      name: "পি বি আই",
-      link: "pbi.html"
+        id: 3,
+        photo: "../resources/icons/fatullah-thana/industrial-police.png",
+        name: "ইন্ডাস্টিয়াল পুলিশ",
+        link: "industrial-police.html"
     },
     {
-      id: 5,
-      photo: "../resources/icons/fatullah-thana/cid.png",
-      name: "সি আই ডি",
-      link: "cid.html"
+        id: 4,
+        photo: "../resources/icons/fatullah-thana/pbi.png",
+        name: "পি বি আই",
+        link: "pbi.html"
     },
     {
-      id: 6,
-      photo: "../resources/icons/fatullah-thana/convicted-missing-accused.png",
-      name: "সাজাপ্রাপ্ত পলাতক আসামি",
-      link: "convicted-missing-accused.html"
+        id: 5,
+        photo: "../resources/icons/fatullah-thana/cid.png",
+        name: "সি আই ডি",
+        link: "cid.html"
+    },
+    {
+        id: 6,
+        photo: "../resources/icons/fatullah-thana/convicted-missing-accused.png",
+        name: "সাজাপ্রাপ্ত পলাতক আসামি",
+        link: "convicted-missing-accused.html"
     }
-  ];
-  
+];
