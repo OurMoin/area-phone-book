78e87d3cbcc3b31d485ecf9dc6c8871658fbbc33
Moin <ourmoin@gmail.com>
2022-04-16 11:32:43 +0600

    team data add

--- fatullah-thana/data/about-usData.js
+++ fatullah-thana/data/about-usData.js
@@ -7,24 +7,24 @@ function userTemplate(user) {
     <!-- Founder & Founder -->
     <div class="col-md-6">
     <div class="justify-content-between align-content-between team-card">
-            <a href="../resources/icons/team/rakibuzzaman.jpg"><img src="../resources/icons/team/rakibuzzaman.jpg" alt="moinul islam" class="team-img"></a>
+            <a href="../resources/icons/teamrakibuzzaman.jpg"><img src="../resources/icons/team/rakibuzzaman.jpg" alt="rakibuzzaman" class="team-img"></a>
             <h2>Rakibuzzaman</h2>
             <p class="text-center">Founder & Host</p>
-            <a href="mailto:usermoin@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
-            <a href="https://github.com/OurMoin" target="_blank"><i class="fab fa-github-square"></i></a>
-            <a href="https://www.facebook.com/OurMoin" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="mailto:ocfatps@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-github-square"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a>
         </div>
     </div>
 
     <!-- Founder & Co-Founder -->
     <div class="col-md-6">
         <div class="justify-content-between align-content-between team-card">
-            <a href="../resources/icons/team/ashik-imran.jpg"><img src="../resources/icons/team/ashik-imran.jpg" alt="sharif imtaz" class="team-img"></a>
+            <a href="../resources/icons/team/ashik-imran.jpg"><img src="../resources/icons/team/ashik-imran.jpg" alt="ashik-imran" class="team-img"></a>
             <h2>Ashik Imran</h2>
             <p class="text-center">Co-Host</p>
-            <a href="mailto:shorifallrobi321@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
-            <a href="https://github.com/sharifimtaz" target="_blank"><i class="fab fa-github-square"></i></a>
-            <a href="https://www.facebook.com/SharifImtaz" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            <a href="mailto:ocfatps@gmail.com" target="_blank"><i class="fas fa-envelope"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-github-square"></i></a>
+            <a href="#" target="_blank"><i class="fab fa-facebook-square"></i></a>
         </div>
     </div>
     <!-- Founder & Founder -->
