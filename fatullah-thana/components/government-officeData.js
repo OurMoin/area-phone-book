01fd97deb668ebee3294ea463e79f5c2e5f1175f
Moin <ourmoin@gmail.com>
2022-04-16 10:48:59 +0600

    logo add

--- fatullah-thana/components/government-officeData.js
+++ fatullah-thana/components/government-officeData.js
@@ -1,69 +1,67 @@
-const categoryData = [
-    {
-      id: 1,
-      photo: "../resources/icons/government-office/emergency-hotlinepolice-super.png",
-      name: "পুলিশ সুপার",
-      link: "police-super.html"
-    },  
-    {
-      id: 2,
-      photo: "../resources/icons/government-office/district-and-sessions-judge-court.png",
-      name: "জেলা ও দায়রা জজ আদালত",
-      link: "district-and-sessions-judge-court.html"
+const categoryData = [{
+        id: 1,
+        photo: "../resources/icons/government-office/bd-police.png",
+        name: "পুলিশ সুপার",
+        link: "police-super.html"
     },
     {
-      id: 3,
-      photo: "../resources/icons/government-office/deputy-commissioner.png",
-      name: "জেলা প্রশাসক",
-      link: "deputy-commissioner.html"
+        id: 2,
+        photo: "../resources/icons/government-office/district-and-sessions-judge-court.png",
+        name: "জেলা ও দায়রা জজ আদালত",
+        link: "district-and-sessions-judge-court.html"
     },
     {
-      id: 4,
-      photo: "../resources/icons/government-office/upazila.png",
-      name: "উপজেলা",
-      link: "upazila.html"
+        id: 3,
+        photo: "../resources/icons/government-office/deputy-commissioner.png",
+        name: "জেলা প্রশাসক",
+        link: "deputy-commissioner.html"
     },
     {
-      id: 5,
-      photo: "../resources/icons/government-office/bgb-(battalion-no-62).png",
-      name: "বিজিবি (৬২নং ব্যাটালিয়ান)",
-      link: "bgb-(battalion-no-62).html"
+        id: 4,
+        photo: "../resources/icons/government-office/upazila.png",
+        name: "উপজেলা",
+        link: "upazila.html"
     },
     {
-      id: 6,
-      photo: "../resources/icons/government-office/prison.png",
-      name: "জেলখান",
-      link: "prison.html"
+        id: 5,
+        photo: "../resources/icons/government-office/bgb-(battalion-no-62).png",
+        name: "বিজিবি (৬২নং ব্যাটালিয়ান)",
+        link: "bgb-(battalion-no-62).html"
     },
     {
-      id: 7,
-      photo: "../resources/icons/government-office/cooperative.png",
-      name: "সমবায়",
-      link: "cooperative.html"
+        id: 6,
+        photo: "../resources/icons/government-office/prison.png",
+        name: "জেলখান",
+        link: "prison.html"
     },
     {
-      id: 8,
-      photo: "../resources/icons/government-office/fisheries-office.png",
-      name: "মৎস্য অফিস",
-      link: "fisheries-office.html"
+        id: 7,
+        photo: "../resources/icons/government-office/cooperative.png",
+        name: "সমবায়",
+        link: "cooperative.html"
     },
     {
-      id: 9,
-      photo: "../resources/icons/government-office/animal-resources.png",
-      name: "প্রাণী সম্পদ",
-      link: "animal-resources.html"
+        id: 8,
+        photo: "../resources/icons/government-office/fisheries-office.png",
+        name: "মৎস্য অফিস",
+        link: "fisheries-office.html"
     },
     {
-      id: 10,
-      photo: "../resources/icons/government-office/land-office.png",
-      name: "ভূমি অফিস",
-      link: "land-office.html"
+        id: 9,
+        photo: "../resources/icons/government-office/animal-resources.png",
+        name: "প্রাণী সম্পদ",
+        link: "animal-resources.html"
     },
     {
-      id: 11,
-      photo: "../resources/icons/government-office/police-line.png",
-      name: "পুলিশ লাইন",
-      link: "police-line.html"
+        id: 10,
+        photo: "../resources/icons/government-office/land-office.png",
+        name: "ভূমি অফিস",
+        link: "land-office.html"
+    },
+    {
+        id: 11,
+        photo: "../resources/icons/government-office/police-line.png",
+        name: "পুলিশ লাইন",
+        link: "police-line.html"
     }
-  ];
-  
+];
