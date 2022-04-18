fb3012148a52bf05d90a077769480d517859bf08
Moin <ourmoin@gmail.com>
2022-04-18 10:05:50 +0600

    data add

--- fatullah-thana/data/kutubpur-union-convicted-cr-warrantsData.js
+++ fatullah-thana/data/kutubpur-union-convicted-cr-warrantsData.js
@@ -1,13 +1,6 @@
 const categoryData = [{
-        id: 1,
-        photo: "resources/icons/fatullah-thana/convicted-cr-warrants.png",
-        name: "বাবুল রানা,  প্রযতেœ ঃ মোঃ ইদ্রিস মিয়া, সাং- মাহমুদপুর  ভ‚ইঘর, থানাঃ- ফতুল্লা, জেলাঃ- নারায়নগঞ্",
-        link: "#"
-    },
-    {
-        id: 2,
-        photo: "resources/icons/fatullah-thana/convicted-cr-warrants.png",
-        name: "চেয়ারম্যান অফিস",
-        link: "#"
-    }
-];
+    id: 1,
+    photo: "resources/icons/fatullah-thana/convicted-cr-warrants.png",
+    name: "বাবুল রানা,  প্রযতে ঃ মোঃ ইদ্রিস মিয়া, সাং- মাহমুদপুর  ভ‚ইঘর, থানাঃ- ফতুল্লা, জেলাঃ- নারায়নগঞ্",
+    link: "#"
+}];
