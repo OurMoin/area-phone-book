b9d41aa57c5f3fe8507c2b9d3545ad9f63b303a6
Shanto <52619179+shantomondol@users.noreply.github.com>
2022-04-26 12:16:47 +0600

    data add in telecom-centerData file

--- data/homeopathyData.js
+++ data/homeopathyData.js
@@ -31,6 +31,17 @@ const userData = [
     detail: "জটিল ও পুরাতন রোগের চিকিৎসা কেন্দ্র।",
    postLink: "http://fb.com/areaPhonebook",
     videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
+  },
+  {
+    id: 4,
+    photo: "../cdn/homeopathy/homeopathy.png",
+    name: "মোহাম্মদিয়া হোমিও ক্লিনিক",
+    number: "01716972307",
+    wordNo: "৬",
+    location: "পাগলা উচ্চ বিদ্যালয় সংলগ্ন",
+    detail: "mahmudur8877@gmail.com<br> চর্ম, যৌন, শিশু ও স্ত্রী রোগ সহ সকল প্রকার জটিল ও কঠিণ রোগের সু চিকিৎসা করা হয়।",
+   postLink: "http://fb.com/areaPhonebook",
+    videoLink: "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fbashundharasnacks%2Fvideos%2F897735790751548%2F&width=500&show_text=false&height=280&appId"
   }
 ];
   
